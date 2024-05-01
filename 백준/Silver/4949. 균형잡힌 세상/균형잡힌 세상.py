d0ef10e3595f16e5a9902c014c7e961ab053311f
JuneKoo Kang <engineerjkk@naver.com>
2024-05-01 19:02:12 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,41 +1,36 @@
 ans=[]
 while True:
+    lst=[]
     s=input()
-    stack=[]
+    balanced=True
     if s==".":
         break
-    balanced=True
     for i in range(len(s)):
         if s[i]=='(' or s[i]=='[':
-            stack.append(s[i])
+            lst.append(s[i])
+        
         if s[i]==')':
-            if len(stack)==0:
+            if len(lst)==0:
                 balanced=False
                 break
-            last=stack.pop(-1)
-
+            last=lst.pop(-1)
             if last!='(':
                 balanced=False
                 break
 
         if s[i]==']':
-            if len(stack)==0:
+            if len(lst)==0:
                 balanced=False
                 break
-            last=stack.pop(-1)
-
+            last=lst.pop(-1)
             if last!='[':
                 balanced=False
                 break
-    if len(stack) !=0:
+    if len(lst)!=0:
         balanced=False
-
     if balanced==True:
         ans.append("yes")
     else:
         ans.append("no")
-
-           
-
 for i in range(len(ans)):
     print(ans[i])
