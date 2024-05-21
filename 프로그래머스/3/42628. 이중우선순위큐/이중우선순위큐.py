9e71363a376b13f673145863c7546fe44b705a29
JuneKoo Kang <engineerjkk@naver.com>
2024-05-21 22:23:24 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,25 @@
+from heapq import heappush,heappop
+
+def solution(operations):
+    heap=[]
+    for i in operations:
+        op,num=i.split()
+        num=int(num)
+        
+        if op=='I':
+            heappush(heap,num)
+            
+        elif op=='D' and num==1:
+            if len(heap)!=0:
+                a=max(heap)
+                heap.remove(a)
+        else:
+            if len(heap)!=0:
+                heappop(heap)
+    if len(heap)==0:
+        answer=[0,0]
+    else:
+        answer=[max(heap),heappop(heap)]
+                
+
+    return answer
