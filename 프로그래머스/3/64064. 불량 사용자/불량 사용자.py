4f82c21a739ff28e17a244659b99f1e8231c4d9e
JuneKoo Kang <engineerjkk@naver.com>
2024-05-22 19:03:21 +0900

    [level 3] Title: 불량 사용자, Time: 5.25 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,27 @@
+from itertools import permutations
+def solution(user_id, banned_id):
+    # 제제아이디 목록은 몇 가지 경우의 수가 가능한가?
+
+    def check(user, ban):
+        if len(user) == len(ban):
+            for i, j in zip(user, ban):
+                if j == '*':
+                    continue
+                elif i != j:
+                    return False
+        else:
+            return False
+        return True
+
+    answer = []
+    for permu in permutations(user_id, len(banned_id)):
+        flag = True
+        for i in range(len(banned_id)):
+            if not check(permu[i], banned_id[i]):
+                flag = False
+
+        if flag:
+            if set(permu) not in answer:
+                answer.append(set(permu))
+
+    return len(answer)
