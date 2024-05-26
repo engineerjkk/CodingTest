ebf76a4ee3e2a32dcc9afcd7ea968448435845f4
JuneKoo Kang <engineerjkk@naver.com>
2024-05-26 13:43:23 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,12 @@
+
+def solution(s):
+    answer = ''
+    for word in s.split(" "):
+        n = ''
+        for i in range(len(word)):
+            if i%2 == 0 :
+                n += word[i].upper()
+            else :
+                n += word[i].lower()
+        answer += (n + " ")
+    return answer[:-1]
