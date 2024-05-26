794cafe58a163147481f67e5f10688b572074d32
JuneKoo Kang <engineerjkk@naver.com>
2024-05-27 00:43:24 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,31 +1,11 @@
 def solution(n, arr1, arr2):
-    answer = []
-    bin_arr1=[]
-    bin_arr2=[]
-    for i in arr1:
-        a=str(bin(i))[2:]
-        b=""
-        if len(a)<n:
-            for _ in range(n-len(a)):
-                b+="0"
-        bin_arr1.append(b+a)
-    for i in arr2:
-        a=str(bin(i))[2:]
-        b=""
-        if len(a)<n:
-            for _ in range(n-len(a)):
-                b+="0"
-        bin_arr2.append(b+a)
-    space=[]
-    for i in range(n):
-        tmp=""
-        for j in range(n):
-            if bin_arr1[i][j]=="1" or bin_arr2[i][j]=="1":
-                tmp+="#"
-            else:
-                tmp+=" "
-        space.append(tmp)
-
+    answer=[]
+    for i,j in zip(arr1,arr2):
+        a=str(bin(i|j))[2:]
+        a=a.rjust(n,"0")
+        a=a.replace("1","#")
+        a=a.replace("0"," ")
+        answer.append(a)
         
 
-    return space
+    return answer
