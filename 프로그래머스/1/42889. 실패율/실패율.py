76c626be87496dadfec150687aa2b65b9180542e
JuneKoo Kang <engineerjkk@naver.com>
2024-05-27 16:57:34 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -2,6 +2,7 @@ def solution(N, stages):
     answer = []
     dic={}
     stages=sorted(stages)
+    len_stages=len(stages)
     for i in range(1,N+1):
         dic[i]=0
     for i in range(1,N+1):
@@ -9,8 +10,9 @@ def solution(N, stages):
             dic[i]=0
         else:
             fail=stages.count(i)
-            dic[i]=fail/len(stages)
+            dic[i]=fail/len_stages
             stages=stages[fail:]
+            len_stages-=fail
     for key,value in sorted(dic.items(),key = lambda x:(x[1],-x[0]),reverse=True):
         answer.append(key)
     
