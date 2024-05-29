ce6af8632cfdee11c4f1446b9cb7f1df98858f08
JuneKoo Kang <engineerjkk@naver.com>
2024-05-29 18:32:35 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,14 @@
+import math
+def solution(brown, yellow):
+    
+    def check(x,y):
+        tmp_y=x*y
+        tmp_b=2*x+2*y+4
+        if tmp_y==yellow and tmp_b==brown:
+            return True
+        else:
+            False
+    for i in range(brown):
+        for j in range(brown):
+            if check(i,j):
+                return sorted([i+2,j+2],reverse=True)
