a6e8b86d03ff39b2dc2d5fb37afe037c03163003
JuneKoo Kang <engineerjkk@naver.com>
2024-05-29 21:40:35 +0900

    [level 2] Title: 예상 대진표, Time: 348.98 ms, Memory: 22.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -4,10 +4,11 @@ def solution(n,a,b):
     lst=[0]*n
     lst[a]=1
     lst[b]=1
-    ans=0
-    while len(lst)>=2:
-        i=0
+    
+    answer=0
+    while n>=2:
         tmp=[]
+        i=0
         while i<n-1:
             if lst[i]==0 and lst[i+1]==0:
                 tmp.append(0)
@@ -15,11 +16,12 @@ def solution(n,a,b):
                 tmp.append(1)
             elif lst[i]==1 and lst[i+1]==0:
                 tmp.append(1)
-            elif lst[i]==1 and lst[i+1]==1:
-                return ans+1
+            else:
+                return answer+1
             i+=2
+        answer+=1
         lst=tmp
         n=len(tmp)
-        #a,b=tmp.index(1)
-        a,b=list(filter(lambda x: tmp[x] == 1, range(len(tmp))))
-        ans+=1
+            
+            
+        
