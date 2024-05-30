82471da926753c0ae5ee54cab24d5bc36384bb10
JuneKoo Kang <engineerjkk@naver.com>
2024-05-31 02:19:45 +0900

    [Silver I] Title: 기타 레슨, Time: 484 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -2,25 +2,22 @@ import sys
 input = sys.stdin.readline
 n,m = map(int,input().split())
 lst=list(map(int,input().split()))
-
 left=max(lst)
 right=sum(lst)
 
 while left<=right:
     mid=(left+right)//2
-    total=0
-
-    num=1
     remain=mid
+    num=1
     for i in range(n):
         if remain<lst[i]:
-            num+=1
             remain=mid
+            num+=1
         remain-=lst[i]
-    if num<=m:
+    
+    if num>m:
+        left=mid+1
+    else:
         right=mid-1
         answer=mid
-    else:
-        left=mid+1  
 print(answer)
-
