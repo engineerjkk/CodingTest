23079980fe704f9805a00b44902661193c6f1f7c
JuneKoo Kang <engineerjkk@naver.com>
2024-05-31 20:24:49 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 148 ms, Memory: 34676 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -4,8 +4,10 @@ t=int(input())
 answer=[]
 for _ in range(t):
     n,m=map(int,input().split())
-    lst_n=sorted(list(map(int,input().split())))
-    lst_m=sorted(list(map(int,input().split())))
+    lst_n=list(map(int,input().split()))
+    lst_m=list(map(int,input().split()))
+    lst_n.sort()
+    lst_m.sort()
     cnt=0
     main=0
     sub=0
@@ -15,8 +17,8 @@ for _ in range(t):
             main+=1
         else:
             if lst_n[main]<=lst_m[sub]:
-                main+=1
                 cnt+=sub
+                main+=1
             else:
                 sub+=1
     answer.append(cnt)
