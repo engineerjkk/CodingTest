dc47656d22728d492d9fbe4a28e5120a1e1d8733
JuneKoo Kang <engineerjkk@naver.com>
2024-05-31 14:03:54 +0900

    [Silver I] Title: 신입 사원, Time: 5676 ms, Memory: 50724 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -2,20 +2,20 @@ import sys
 input = sys.stdin.readline
 t=int(input())
 answer=[]
-
 for _ in range(t):
     n=int(input())
     lst=[]
     for _ in range(n):
         lst.append(list(map(int,input().split())))
+    
     lst.sort()
     MAX=sys.maxsize
     cnt=0
     for i in range(n):
         if lst[i][1]<MAX:
             cnt+=1
-            MAX=min(MAX,lst[i][1])
+            MAX=min(lst[i][1],MAX)
     answer.append(cnt)
+
 for i in answer:
     print(i)
-
