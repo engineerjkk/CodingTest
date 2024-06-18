d3e228d179e96f539117cc0a220c7e55d585f841
JuneKoo Kang <engineerjkk@naver.com>
2024-06-18 13:55:10 +0900

    [level 2] Title: 구명보트, Time: 7.87 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,19 @@
-import sys
 def solution(people, limit):
-    start=0
-    end=len(people)-1
-    answer=0
-    people=sorted(people)
-    while start<=end:
-        if people[start]+people[end]<=limit:
-            start+=1
-            end-=1
-            answer+=1
-        else:
-            end-=1
-            answer+=1
-    return answer
-        
+    answer = 0
+    people.sort()  # 사람들의 몸무게를 오름차순으로 정렬
+
+    a = 0  # 가장 가벼운 사람을 가리키는 포인터
+    b = len(people) - 1  # 가장 무거운 사람을 가리키는 포인터
+    while a <= b:
+        if people[a] + people[b] <= limit:
+            # 가장 가벼운 사람(a)과 가장 무거운 사람(b)을 함께 보트에 태울 수 있는 경우
+            a += 1  # 가벼운 사람을 다음 사람으로 이동
+        # 무거운 사람은 항상 보트에 태움
+        b -= 1  # 무거운 사람을 다음 무거운 사람으로 이동
+        answer += 1  # 필요한 보트의 수 증가
+
+    return answer  # 모든 사람을 구출하는 데 필요한 보트의 최솟값 반환
+
+# 예시 테스트 케이스
+print(solution([70, 50, 80, 50], 100))  # 출력: 3
+print(solution([70, 80, 50], 100))  # 출력: 3
