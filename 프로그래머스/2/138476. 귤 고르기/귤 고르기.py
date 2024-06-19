1b7eab0a58e04b51b39dee2c1cf82382788573c3
JuneKoo Kang <engineerjkk@naver.com>
2024-06-19 20:28:23 +0900

    [level 2] Title: 귤 고르기, Time: 34.27 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,17 +1,16 @@
 def solution(k, tangerine):
-    answer=0
     dic={}
     for i in tangerine:
         if i in dic:
             dic[i]+=1
         else:
             dic[i]=1
-    
-    dic=sorted(dic.items(),key=lambda x:x[1], reverse=True)
+    dic=sorted(dic.items(),key=lambda x:x[1],reverse=True)
+    total=0
+    answer=0
     for key,value in dic:
         k-=value
         answer+=1
         if k<=0:
             break
-
     return answer
