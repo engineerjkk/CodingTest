7fbe708705dc27cff8b034b49da6aa1748b3d86f
JuneKoo Kang <engineerjkk@naver.com>
2024-06-19 18:05:52 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -2,12 +2,14 @@ def solution(brown, yellow):
     
     def check(x,y):
         tmp_y=x*y
-        tmp_b=2*x+2*y+4
+        tmp_b=(x+2)*(y+2)-x*y
         if tmp_y==yellow and tmp_b==brown:
             return True
-        else:
-            False
+        else: 
+            return False
+    
     for i in range(brown):
         for j in range(brown):
             if check(i,j):
                 return sorted([i+2,j+2],reverse=True)
+    
