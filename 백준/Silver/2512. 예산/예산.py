d93b080b682a0b6f2483c0666bfe4c418bd3db4c
JuneKoo Kang <engineerjkk@naver.com>
2024-06-21 13:04:17 +0900

    [Silver II] Title: 예산, Time: 72 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -3,18 +3,18 @@ input = sys.stdin.readline
 n=int(input())
 lst=list(map(int,input().split()))
 limit=int(input())
+
 left=0
 right=max(lst)
-lst.sort()
+answer=0
 while left<=right:
     mid=(left+right)//2
     total=0
-    for i in range(n):
-        total+=min(mid,lst[i])
+    for i in lst:
+        total+=min(mid,i)
     if total<=limit:
         answer=mid
         left=mid+1
     else:
         right=mid-1
 print(answer)
-    
