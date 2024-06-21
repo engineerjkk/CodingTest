d0c75a567f5a5dbcb8de84608095ba8110aefeb7
JuneKoo Kang <engineerjkk@naver.com>
2024-06-21 13:27:42 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,7 +1,7 @@
 def solution(citations):
+    n=len(citations)
     citations.sort(reverse=True)
-    #6 5 3 1 0
-    for i in range(len(citations)):
-        if i >=citations[i]:
+    for i in range(n):
+        if i>=citations[i]:
             return i
-    return len(citations)
+    return n
