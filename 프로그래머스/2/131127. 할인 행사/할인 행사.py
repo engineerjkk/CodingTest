d3ce36ffdacdeef082ab0d0d92ca957f2ae94e7d
JuneKoo Kang <engineerjkk@naver.com>
2024-06-21 13:25:17 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -7,5 +7,7 @@ def solution(want, number, discount):
     for i in range(len(discount)-9):
         if dic==Counter(discount[i:i+10]):
             answer+=1
-    return answer        
+    return answer
+    
+      
  
