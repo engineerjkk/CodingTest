d7f6829d40afb7fd384d6004c5bbf5cce1f42de6
JuneKoo Kang <engineerjkk@naver.com>
2024-06-23 16:58:56 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,31 +1,35 @@
-def n_to_k(n,k):
+def transform(num,k):
+    numOver={10:"A",11:"B",12:"C",13:"D",14:"E",15:"F"}
     ret=""
-    while n>0:
-        ret+=str(n%k)
-        n=n//k
+    while num>0:
+        remainder=num%k
+        if remainder>=10:
+            ret+=numOver[remainder]
+        else:
+            ret+=str(remainder)
+        num=num//k
     return ret[::-1]
 
-def check_prime(n):
-    if n==2 or n==3:
+def check_prime(num):
+    if num==2 or num==3:
         return True
-    if n%2==0 or n<2:
+    if num%2==0 or num<2:
         return False
-    for i in range(3,int(n**(0.5))+1,2):
-        if n%i==0:
+    for i in range(3,int(num**(0.5))+1):
+        if num%i==0:
             return False
     return True
+        
 
 def solution(n, k):
-    num_k=n_to_k(n,k)
-    num_k=num_k.split("0")
+    k_num=transform(n,k)
+    k_num=k_num.split("0")
     answer=0
-    for i in num_k:
+    for i in k_num:
         if i=="":
             continue
         if check_prime(int(i)):
             answer+=1
     return answer
-        
-
 
 
