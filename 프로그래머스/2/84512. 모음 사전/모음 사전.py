af1933565e175a990b5790fc951ca8de2f6fcf4c
JuneKoo Kang <engineerjkk@naver.com>
2024-06-24 15:34:29 +0900

    [level 2] Title: 모음 사전, Time: 1.84 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,10 @@
 from itertools import product
-
 def solution(word):
-    words = []
-    for i in range(1, 6):
-        for c in product(['A', 'E', 'I', 'O', 'U'], repeat=i):
-            words.append(''.join(list(c)))
-
-    words.sort()
-    return words.index(word) + 1
+    words=["A","E","I","O","U"]
+    tmp=[]
+    for i in range(1,6):
+        for x in list(product(words,repeat=i)):
+            #print("".join(list(x)))
+            tmp.append("".join(list(x)))
+    tmp.sort()
+    return tmp.index(word)+1
