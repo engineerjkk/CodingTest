15dd240f9d1c4988c3dd3b4b4c101d8dd6818557
JuneKoo Kang <engineerjkk@naver.com>
2024-06-24 16:29:43 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,14 +1,14 @@
 def solution(s):
     s=s[2:-2].split("},{")
-    answer=[]
     tmp=[]
     for i in s:
         tmp.append(list(map(int,i.split(","))))
     tmp.sort(key=lambda x:len(x))
+    ans=[]
     for i in range(len(tmp)):
         for j in tmp[i]:
-            if j not in answer:
-                answer.append(j)
-    return answer
+            if j not in ans:
+                ans.append(j)
+    return ans
 
                    
