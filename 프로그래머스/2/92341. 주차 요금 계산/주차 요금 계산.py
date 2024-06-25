8ffa53b674e25f5da9f2cc7ababc11bd76b22784
JuneKoo Kang <engineerjkk@naver.com>
2024-06-25 23:36:10 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -3,37 +3,36 @@ from math import ceil
 class Parking:
     def __init__(self,fees):
         self.fees=fees
-        self.in_flag=False
-        self.in_time=0
+        self.flag=False
+        self.time=0
         self.total=0
     def update(self,t,inout):
         if inout=="IN":
-            self.in_flag=True
+            self.flag=True
         else:
-            self.in_flag=False
-        if self.in_flag:
-            self.in_time=str2int(t)
+            self.flag=False
+        if self.flag:
+            self.time=str2int(t)
         else:
-            self.total+=(str2int(t)-self.in_time)
-    def calc_fee(self):
-        if self.in_flag:
+            self.total+=(str2int(t)-self.time)
+    def cal_fee(self):
+        if self.flag:
             self.update("23:59","out")
-        add_t=self.total-self.fees[0]
-        if add_t>=0:
-            return self.fees[1]+ceil(add_t/self.fees[2])*self.fees[3]
+        add_time=self.total-self.fees[0]
+        if add_time>=0:
+            return self.fees[1]+ceil(add_time/self.fees[2])*self.fees[3]
         else:
             return self.fees[1]
-
+    
 def str2int(string):
     return int(string[:2])*60+int(string[3:])
 
-            
-def solution(fees, records):
+def solution(fees,records):
     dic=defaultdict(lambda:Parking(fees))
     for record in records:
-        t,car,inout=record.split()
-        dic[car].update(t,inout)
+        time, car, inout = record.split()
+        dic[car].update(time,inout)
     answer=[]
     for key,value in sorted(dic.items()):
-        answer.append(value.calc_fee())
+        answer.append(dic[key].cal_fee())
     return answer
