6a5dbe500fae67ce318dbae4ec4be3254a698347
JuneKoo Kang <engineerjkk@naver.com>
2024-06-25 20:16:47 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,45 +1,39 @@
-import math
+from collections import defaultdict
+from math import ceil
+class Parking:
+    def __init__(self,fees):
+        self.fees=fees
+        self.in_flag=False
+        self.in_time=0
+        self.total=0
+    def update(self,t,inout):
+        if inout=="IN":
+            self.in_flag=True
+        else:
+            self.in_flag=False
+        if self.in_flag:
+            self.in_time=str2int(t)
+        else:
+            self.total+=(str2int(t)-self.in_time)
+    def calc_fee(self):
+        if self.in_flag:
+            self.update("23:59","out")
+        add_t=self.total-self.fees[0]
+        if add_t>=0:
+            return self.fees[1]+ceil(add_t/self.fees[2])*self.fees[3]
+        else:
+            return self.fees[1]
+
+def str2int(string):
+    return int(string[:2])*60+int(string[3:])
+
+            
 def solution(fees, records):
-    answer = []
-    # fees 설명
-    # 기본 시간, 기본 요금 , 몇분당? , 추가요금
-    basic_time , basic_fee , per_min , plus_fee = fees
-
-    car = {}
-    for idx in records:
-        idx = idx.split()
-        car[idx[1]] = 0
-
-    dic = {}
-    for idx in records:
-        idx = idx.split()
-        temp = idx[0].split(':')
-        in_time = int(temp[0])*60 + int(temp[1])
-
-        if idx[2] == 'IN':
-            dic[idx[1]] = in_time
-
-        elif idx[2] == 'OUT':
-            if idx[1] in dic.keys():
-                car[idx[1]] += in_time - dic[idx[1]]
-                dic[idx[1]] = -1
-
-    for key,value in dic.items():
-        if value != -1: # 입차는 했는데 출차를 안한 경우임.
-            car[key] = car[key] + (1439 - dic[key])
-
-
-    for key,value in car.items():
-        if value <= basic_time: # 기본 시간보다 아래인 누적 시간인 경우
-            answer.append((int(key),basic_fee)) # 그냥 기본 요금만 받아
-
-        elif value > basic_time: # 오버 되었으면?
-            tot_fee = basic_fee + math.ceil((value - basic_time)/per_min) * plus_fee
-            answer.append((int(key),tot_fee))
-
-    answer.sort(key=lambda x : x[0]) # 차번호 기준으로 오름차순 정렬
-    real_answer = []
-    for key,money in answer:
-        real_answer.append(money)
-
-    return real_answer
+    dic=defaultdict(lambda:Parking(fees))
+    for record in records:
+        t,car,inout=record.split()
+        dic[car].update(t,inout)
+    answer=[]
+    for key,value in sorted(dic.items()):
+        answer.append(value.calc_fee())
+    return answer
