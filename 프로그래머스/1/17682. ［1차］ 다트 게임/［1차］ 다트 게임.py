494fccd272e0aaa523291cfac8c19eddb49003d7
JuneKoo Kang <engineerjkk@naver.com>
2024-06-25 18:26:28 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,29 +1,28 @@
 def solution(dartResult):
-    answer = 0
-    lst=[]
-    num=0
-    dartResult=dartResult.replace("10","-")
-    for i,v in enumerate(list(dartResult.strip())):
-        if ord('0')<=ord(v)<=ord('9'):
-            num=int(v)
-        elif v=="-":
-            num=10
-        elif v=="S":
-            tmp=num
-            lst.append(tmp)
-        elif v=="D":
-            tmp=num**2
-            lst.append(tmp)
-        elif v=="T":
-            tmp=num**3
-            lst.append(tmp)
-        elif v=="#":
-            lst[-1]=lst[-1]*(-1)
-        elif v=="*":
-            if len(lst)>=2:
-                lst[-2]=lst[-2]*2
-                lst[-1]=lst[-1]*2
-            else:
-                lst[-1]=lst[-1]*2
-    print(lst)
-    return sum(lst)
+    result=[]
+    stack=[]
+    i=0
+    num=""
+    for i in dartResult:
+        if i.isdecimal():
+            num+=i
+        if i.isalpha():
+            result.append(int(num))
+            num=""
+            if i=="D":
+                result[-1]=result[-1]**2
+            elif i=="T":
+                result[-1]=result[-1]**3
+            continue
+        else:
+            if i=="*":
+                if len(result)>1:
+                    result[-1]=result[-1]*2
+                    result[-2]=result[-2]*2
+                elif len(result)==1:
+                    result[-1]=result[-1]*2
+            elif i=="#":
+                result[-1]=-result[-1]
+            continue
+        
+    return int(sum(result))
