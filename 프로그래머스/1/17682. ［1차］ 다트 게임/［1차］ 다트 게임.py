042efcc7d9b4f420fc3d437d9ce085312f8f7271
JuneKoo Kang <engineerjkk@naver.com>
2024-06-26 11:38:31 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.05 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,28 +1,21 @@
 def solution(dartResult):
     result=[]
-    stack=[]
-    i=0
     num=""
     for i in dartResult:
         if i.isdecimal():
             num+=i
-        if i.isalpha():
+        elif i.isalpha():
             result.append(int(num))
             num=""
             if i=="D":
                 result[-1]=result[-1]**2
             elif i=="T":
                 result[-1]=result[-1]**3
-            continue
         else:
             if i=="*":
+                result[-1]=result[-1]*2
                 if len(result)>1:
-                    result[-1]=result[-1]*2
                     result[-2]=result[-2]*2
-                elif len(result)==1:
-                    result[-1]=result[-1]*2
             elif i=="#":
                 result[-1]=-result[-1]
-            continue
-        
-    return int(sum(result))
+    return sum(result)
