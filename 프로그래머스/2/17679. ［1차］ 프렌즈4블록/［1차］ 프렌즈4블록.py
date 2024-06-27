be9b193b826cb41afcf4d9436cc9624d24dafbf6
JuneKoo Kang <engineerjkk@naver.com>
2024-06-27 13:39:23 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 6.75 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,46 +1,38 @@
-def check(m, n, board):
-    filter = [[0 for _ in range(n)] for _ in range(m)]
-    count = 0
+def check(n,m,board):
+    tmp=[[0]*m for _ in range(n)]
+    for r in range(n-1):
+        for c in range(m-1):
+            if board[r][c]==board[r][c+1]==board[r+1][c]==board[r+1][c+1] and board[r][c]!="0":
+                tmp[r][c]=1
+                tmp[r][c+1]=1
+                tmp[r+1][c]=1
+                tmp[r+1][c+1]=1
+    cnt=0
+    for r in range(n):
+        for c in range(m):
+            if tmp[r][c]==1:
+                cnt+=1
+                board[r][c]="0"
     
-    # check can delete
-    for i in range(m-1):
-        for j in range(n-1):
-            a = board[i][j]
-            b = board[i][j+1]
-            c = board[i+1][j]
-            d = board[i+1][j+1]
-            if a == b == c == d and a != '0':
-                filter[i][j], filter[i][j+1], filter[i+1][j], filter[i+1][j+1] = 1, 1, 1, 1
-    
-    for i in range(m):
-        for j in range(n):
-            if filter[i][j] == 1:
-                count += 1
-                board[i][j] = '0'
-    
-    if count == 0:
+    if cnt==0:
         return 0
     
-    # fill blank
-    for i in range(m-2, -1, -1):
-        for j in range(n):
-            k = i
-            while 0 <= k+1 < m and board[k+1][j] == '0':
-                k += 1
-            if k != i:
-                board[k][j] = board[i][j]
-                board[i][j] = '0'
-                
-    return count
+    for r in range(n-2,-1,-1):
+        for c in range(m):
+            t=r
+            while -1<t+1<n and board[t+1][c]=="0":
+                t+=1
+            if t!=r:
+                board[t][c]=board[r][c]
+                board[r][c]="0"
+    return cnt
 
-def solution(m, n, board):
+def solution(n, m, board):
     answer = 0
-    board = list(map(list, board))
-    
+    board=list(map(list,board))
     while True:
-        temp = check(m, n, board)
-        if temp == 0:
+        cnt=check(n,m,board)
+        if cnt==0:
             break
-        answer += temp
-        
+        answer+=cnt
     return answer
