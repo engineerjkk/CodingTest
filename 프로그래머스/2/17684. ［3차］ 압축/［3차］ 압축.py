67b0a12fdbc4fa87e06df23c1be0dea7851b158c
JuneKoo Kang <engineerjkk@naver.com>
2024-06-27 12:56:03 +0900

    [level 2] Title: [3차] 압축, Time: 0.28 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,18 +1,17 @@
 def solution(msg):
+    answer = []
     dic={}
     for i in range(26):
         dic[chr(ord("A")+i)]=i+1
     
     w,c=0,0
-    answer=[]
     while True:
         c+=1
         if c==len(msg):
-            answer.append(dic[msg[w:c]])
+            answer.append(dic[msg[w:c+1]])
             break
         if msg[w:c+1] not in dic:
             dic[msg[w:c+1]]=len(dic)+1
             answer.append(dic[msg[w:c]])
             w=c
     return answer
-        
