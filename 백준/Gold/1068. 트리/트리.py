19db1fe285583586a85bb692c0832e1d9332b0e9
JuneKoo Kang <engineerjkk@naver.com>
2024-06-28 23:45:04 +0900

    [Gold V] Title: 트리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -8,7 +8,6 @@ for i in range(n):
     if parent[i]==-1:
         root=i
         break
-
 black=[0]*n
 for i in range(n):
     u=i
@@ -18,6 +17,7 @@ for i in range(n):
             break
         if u==root:
             break
+
         u=parent[u]
 
 red=[0]*n
