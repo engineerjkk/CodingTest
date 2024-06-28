455626cfb16b7bf749206ec62b3d73a76a16cc17
JuneKoo Kang <engineerjkk@naver.com>
2024-06-29 02:09:07 +0900

    [Gold IV] Title: 회사 문화 1, Time: 172 ms, Memory: 41844 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -1,11 +1,14 @@
-n,m=list(map(int,input().split()))
+import sys
+input = sys.stdin.readline
+n,m=map(int,input().split())
 parent=list(map(int,input().split()))
-for i in range(1,n):
+
+for i in range(n):
     parent[i]-=1
 
 good=[0]*n
-for _ in range(m):
-    id,score=list(map(int,input().split()))
+for i in range(m):
+    id,score=map(int,input().split())
     good[id-1]+=score
 
 total_good=[0]*n
