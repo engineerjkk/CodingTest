102a383f3c2ac54c2f6556d9ddf8af0ffe46ef56
JuneKoo Kang <engineerjkk@naver.com>
2024-07-01 01:43:05 +0900

    [Platinum III] Title: 가장 긴 문자열, Time: 3560 ms, Memory: 84520 KB -BaekjoonHub

--- 백준/Platinum/3033. 가장 긴 문자열/가장 긴 문자열.py
+++ 백준/Platinum/3033. 가장 긴 문자열/가장 긴 문자열.py
@@ -1,34 +1,32 @@
-L=int(input())
-S=input()
-
+import sys
+input = sys.stdin.readline
+n=int(input())
+s=str(input())
 mod=1e9+7
-po=[0]*L
+po=[0]*n
 po[0]=1
-for i in range(1,L):
-    po[i]=po[i-1]*31%mod
+for i in range(1,n):
+    po[i]=(po[i-1]*31)%mod
 
-low=1
-high=L-1
+left=1
+right=n-1
 answer=0
-
-while low<=high:
-    mid=(low+high)//2
+while left<=right:
+    mid=(left+right)//2
     found=False
 
-    # 가장 앞에 있는 길이 mid인 부분 문자열의 해쉬값
     hash=0
     for i in range(mid):
         hash*=31
         hash%=mod
-        hash+=ord(S[i])-ord('a')+1
+        hash+=ord(s[i])-ord("a")+1
         hash%=mod
+    
     check={}
-
-    for i in range(0,L-mid+1):
-        #S[i:i+mid] 문자열의 해쉬값을 체크
+    for i in range(n-mid+1):
         if hash in check:
             for j in check[hash]:
-                if S[j:j+mid]==S[i:i+mid]:
+                if s[j:j+mid]==s[i:i+mid]:
                     found=True
                     break
             check[hash].append(i)
@@ -37,18 +35,15 @@ while low<=high:
         else:
             check[hash]=[i]
 
-        #해쉬값을 갱신
-        hash-=((ord(S[i]) - ord("a")+1)*po[mid-1])%mod
+        hash-=((ord(s[i])-ord("a")+1)*po[mid-1])%mod
         hash*=31
-        hash%=mod
-        if i+mid<L:
-            hash+=ord(S[i+mid])-ord("a")+1
+        if i+mid<n:
+            hash+=(ord(s[i+mid])-ord("a")+1)
             hash%=mod
 
     if found:
         answer=mid
-        low=mid+1
+        left=mid+1
     else:
-        high=mid-1
-
+        right=mid-1
 print(answer)
