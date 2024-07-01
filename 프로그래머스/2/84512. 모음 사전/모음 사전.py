bc5002e2e1403d70d48467a6ba0e95520f386d87
JuneKoo Kang <engineerjkk@naver.com>
2024-07-01 12:11:15 +0900

    [level 2] Title: 모음 사전, Time: 1.63 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -3,7 +3,7 @@ def solution(word):
     lst=["A","E","I","O","U"]
     answer=[]
     for i in range(1,6):
-        for x in list(product(lst,repeat=i)):
-            answer.append("".join(x))
+        for pro in product(lst,repeat=i):
+            answer.append("".join(pro))
     answer.sort()
     return answer.index(word)+1
