247bf34835caf856d81557f47e3619da17d13fad
JuneKoo Kang <engineerjkk@naver.com>
2024-07-01 15:04:29 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 296 ms, Memory: 49852 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+
 n=int(input())
 lst=[[] for _ in range(n+1)]
 for _ in range(n-1):
@@ -7,8 +8,6 @@ for _ in range(n-1):
     lst[a].append(b)
     lst[b].append(a)
 
-visit=[False]*(n+1)
-answer=[0]*(n+1)
 def dfs(i):
     stack=[i]
     visit[i]=True
@@ -17,8 +16,12 @@ def dfs(i):
         for j in lst[node]:
             if not visit[j]:
                 visit[j]=True
-                answer[j]=node
                 stack.append(j)
+                answer[j]=node
+    
+
+answer=[0]*(n+1)
+visit=[False]*(n+1)
 root=1
 dfs(root)
 for i in range(2,n+1):
