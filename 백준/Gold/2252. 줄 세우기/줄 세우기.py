36a367c3b1e802f2107739f35e896ac8cb63f68c
JuneKoo Kang <engineerjkk@naver.com>
2024-07-02 15:58:13 +0900

    [Gold III] Title: 줄 세우기, Time: 224 ms, Memory: 38744 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n,m=map(int,input().split())
+lst=[[] for _ in range(n)]
+tmp=[0]*n
+
+for i in range(m):
+    a,b=map(int,input().split())
+    lst[a-1].append(b-1)
+    tmp[b-1]+=1
+
+queue=deque()
+for i in range(n):
+    if tmp[i]==0:
+        queue.append(i)
+
+answer=[]
+while queue:
+    u=queue.popleft()
+    answer.append(u)
+    for i in lst[u]:
+        tmp[i]-=1
+        if tmp[i]==0:
+            queue.append(i)
+for i in range(n):
+    print(answer[i]+1,end=" ")
