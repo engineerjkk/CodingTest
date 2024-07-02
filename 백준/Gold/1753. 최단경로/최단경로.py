57345970a2733f61839150baf69327fd58063dfc
JuneKoo Kang <engineerjkk@naver.com>
2024-07-02 11:58:59 +0900

    [Gold IV] Title: 최단경로, Time: 556 ms, Memory: 67356 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -3,12 +3,10 @@ import heapq
 input = sys.stdin.readline
 V,E=map(int,input().split())
 K=int(input())
-
 graph=[[] for _ in range(V+1)]
 for _ in range(E):
     u,v,w=map(int,input().split())
     graph[u].append((v,w))
-
 distance=[1e9]*(V+1)
 
 def dijkstra(start):
@@ -29,8 +27,9 @@ def dijkstra(start):
 
 dijkstra(K)
 
-for num in range(1,V+1):
-    if distance[num]==1e9:
+for i in range(1,V+1):
+    if distance[i]==1e9:
         print("INF")
     else:
-        print(distance[num])
+        print(distance[i])
+
