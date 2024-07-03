deb1b99d1450d646c487ec9232e257d8b6de4961
JuneKoo Kang <engineerjkk@naver.com>
2024-07-03 23:40:58 +0900

    [Silver III] Title: Four Squares, Time: 132 ms, Memory: 110764 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,13 +1,13 @@
 import sys
 input = sys.stdin.readline
 n=int(input())
-dp=[4]*(n+1)
 
-for i in range(1,n+1):
+dp=[4]*(n+1)
+for i in range(n+1):
     if (i**0.5).is_integer():
         dp[i]=1
         continue
-    for j in range(n):
+    for j in range(n+1):
         if j*j>i:
             break
         dp[i]=min(dp[i],1+dp[i-j*j])
