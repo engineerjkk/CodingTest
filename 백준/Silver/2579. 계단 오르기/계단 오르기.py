2b71dbd890e8cd834efb4f76f3ed0a2d57b99787
JuneKoo Kang <engineerjkk@naver.com>
2024-07-03 13:45:25 +0900

    [Silver III] Title: 계단 오르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
 n=int(input())
-s=[0]*n
-for i in range(n):
-    s[i]=int(input())
+s=[]
+for _ in range(n):
+    s.append(int(input()))
 
 a=[0]*n
 b=[0]*n
-
 a[0]=s[0]
 b[0]=s[0]
 
@@ -20,4 +19,4 @@ for i in range(1,n):
         b[i]=s[i]+s[i-1]+max(a[i-3],b[i-3])
     else:
         b[i]=s[i]+s[i-1]
-print(max(a[n-1],b[n-1]))
+print(max(a[-1],b[-1]))
