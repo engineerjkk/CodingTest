efcbc3d3ec97add46c2786c663e8033af2a57b0f
JuneKoo Kang <engineerjkk@naver.com>
2024-07-04 13:22:57 +0900

    [Gold II] Title: 문제집, Time: 188 ms, Memory: 116892 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -2,27 +2,25 @@ import sys
 input = sys.stdin.readline
 import heapq
 n,m=map(int,input().split())
-adj=[[] for _ in range(n)]
-
-need_to_learn=[0]*n
+lst=[[] for _ in range(n)]
+tmp=[0]*n
 for _ in range(m):
     a,b=map(int,input().split())
-    adj[a-1].append(b-1)
-    need_to_learn[b-1]+=1
+    lst[a-1].append(b-1)
+    tmp[b-1]+=1
 
 pq=[]
 for i in range(n):
-    if need_to_learn[i]==0:
+    if tmp[i]==0:
         heapq.heappush(pq,i)
-
-learn=[]
+answer=[]
 while pq:
-    u=heapq.heappop(pq)
-    learn.append(u)
-    for v in adj[u]:
-        need_to_learn[v]-=1
-        if need_to_learn[v]==0:
-            heapq.heappush(pq,v)
+    q=heapq.heappop(pq)
+    answer.append(q)
+    for i in lst[q]:
+        tmp[i]-=1
+        if tmp[i]==0:
+            heapq.heappush(pq,i)
+for i in answer:
+    print(i+1,end=" ")
 
-for i in range(n):
-    print(learn[i]+1,end=" ")
