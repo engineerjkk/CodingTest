befe448a6eacb8b51835651ba7948198ca0a92c0
JuneKoo Kang <engineerjkk@naver.com>
2024-07-04 18:29:54 +0900

    [Silver I] Title: 스타트와 링크, Time: 5436 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -2,32 +2,30 @@ import sys
 input = sys.stdin.readline
 from itertools import combinations
 n=int(input())
-space=[]
-for _ in range(n):
-    space.append(list(map(int,input().split())))
-
 lst=[]
-for i in range(n):
-    lst.append(i)
+for _ in range(n):
+    lst.append(list(map(int,input().split())))
 
+tmp=[]
+for i in range(n):
+    tmp.append(i)
 MIN=sys.maxsize
-for nCr in combinations(lst,n//2):
+for nCr in combinations(tmp,n//2):
     start=list(nCr)
     link=[]
     for i in range(n):
         if i not in start:
             link.append(i)
-    start_sum=0
-    link_sum=0
-
+    
+    startSum=0
+    linkSum=0
+    
     for i in start:
         for j in start:
-            start_sum+=space[i][j]
-    
+            startSum+=lst[i][j]
     for i in link:
         for j in link:
-            link_sum+=space[i][j]
-    MIN=min(MIN,abs(start_sum-link_sum))
+            linkSum+=lst[i][j]
+    MIN=min(MIN,abs(startSum-linkSum))
 print(MIN)
-
 
