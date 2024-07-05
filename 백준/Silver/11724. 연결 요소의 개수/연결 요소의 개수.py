9b6e415fba37957f13e2bedcd802b779f9761f5d
JuneKoo Kang <engineerjkk@naver.com>
2024-07-05 23:51:24 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 408 ms, Memory: 173292 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,28 +1,46 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-n,m = map(int,input().split())
+n,m=map(int,input().split())
 lst=[[] for _ in range(n)]
 for _ in range(m):
     a,b=map(int,input().split())
     lst[a-1].append(b-1)
     lst[b-1].append(a-1)
-
 visit=[False]*n
 
-def bfs(start):
+def bfs(i):
     queue=deque()
-    queue.append(start)
-    visit[start]=True
+    queue.append(i)
+    visit[i]=True
+
     while queue:
         q=queue.popleft()
-        for i in lst[q]:
-            if not visit[i]:
-                visit[i]=True
-                queue.append(i)
-cnt=0
+        for j in lst[q]:
+            if not visit[j]:
+                visit[j]=True
+                queue.append(j)
+
+def dfs(i):
+    stack=[i]
+    visit[i]=True
+    while stack:
+        node=stack.pop()
+        for j in lst[node]:
+            if not visit[j]:
+                visit[j]=True
+                stack.append(j)
+
+def dfs_recursive(i):
+    visit[i]=True
+    
+    for j in lst[i]:
+        if not visit[j]:
+            dfs_recursive(j)
+
+answer=0
 for i in range(n):
     if visit[i]==False:
-        bfs(i)
-        cnt+=1
-print(cnt)
+        dfs_recursive(i)
+        answer+=1
+print(answer)
