49b838677d3201ca080834fecb557da97ce99205
JuneKoo Kang <engineerjkk@naver.com>
2024-07-07 22:12:46 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,20 +1,17 @@
 import sys
 input = sys.stdin.readline
 from itertools import combinations
-import heapq
-l,c=map(int,input().split())
-alpha=['a','e','i','o','u']
+L,C=map(int,input().split())
+moum=["a","e","i","o",'u']
 lst=list(map(str,input().split()))
 lst.sort()
-answer=set()
-pq=[]
-for nCr in sorted(combinations(lst,l)):
-    ja=0
+for nCr in combinations(lst,L):
     mo=0
+    ja=0
     for x in nCr:
-        if x not in alpha:
-            ja+=1
-        else:
+        if x in moum:
             mo+=1
-    if ja>=2 and mo>=1:
-            print("".join(nCr))
+        else:
+            ja+=1
+    if mo>=1 and ja>=2:
+        print("".join(list(nCr)))
