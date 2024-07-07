6815890fbb7f99196e7d6d771a045a6e94869b63
JuneKoo Kang <engineerjkk@naver.com>
2024-07-07 22:44:37 +0900

    [Gold V] Title: 토마토, Time: 444 ms, Memory: 164836 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,9 +1,7 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
-
-#m=열, n=행
-m,n = map(int,input().split())
+from collections import deque
+m,n=map(int,input().split())
 space=[]
 for _ in range(n):
     space.append(list(map(int,input().split())))
@@ -13,24 +11,35 @@ for i in range(n):
     for j in range(m):
         if space[i][j]==1:
             queue.append((i,j))
-dr=[1,0,-1,0]
-dc=[0,-1,0,1]
+
+dr=[-1,0,1,0]
+dc=[0,1,0,-1]
+
+def in_range(r,c):
+    return -1<r<n and -1<c<m
+
+visit=[[False]*m for _ in range(n)]
 
 while queue:
     r,c=queue.popleft()
+    visit[r][c]=True
     for i in range(4):
         nr=r+dr[i]
         nc=c+dc[i]
-        if -1<nr<n and -1<nc<m and space[nr][nc]==0:
+        if in_range(nr,nc) and not visit[nr][nc] and space[nr][nc]==0:
             space[nr][nc]=space[r][c]+1
+            visit[nr][nc]=True
             queue.append((nr,nc))
-MAX=0
-for row in space:
-    for i in row:
-        if i == 0:
+
+
+for i in range(n):
+    for j in range(m):
+        if space[i][j]==0:
             print(-1)
             exit()
-    else:
-        MAX=max(MAX,max(row))
-print(MAX-1)
 
+MAX=0
+for i in range(n):
+    for j in range(m):
+        MAX=max(MAX,space[i][j])
+print(MAX-1)
