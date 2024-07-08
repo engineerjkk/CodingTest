729f493dcb9d5540fca89b99f701bb763c3b49e3
JuneKoo Kang <engineerjkk@naver.com>
2024-07-08 22:29:53 +0900

    [Silver IV] Title: 정수 제곱근, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -3,12 +3,11 @@ input = sys.stdin.readline
 n=int(input())
 left=0
 right=2**63
-answer=0
 while left<=right:
     mid=(left+right)//2
     if mid**2<n:
         left=mid+1
     else:
-        answer=mid
+        ans=mid
         right=mid-1
-print(answer)
+print(ans)
