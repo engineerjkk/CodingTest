99eabefa07e1aa7fb683769344f2fdfa55620126
JuneKoo Kang <engineerjkk@naver.com>
2024-07-08 22:40:55 +0900

    [Silver IV] Title: 로프, Time: 124 ms, Memory: 114416 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -4,8 +4,9 @@ n=int(input())
 lst=[]
 for _ in range(n):
     lst.append(int(input()))
+
 lst.sort(reverse=True)
 MAX=0
-for i in range(1,len(lst)+1):
-    MAX=max(MAX,lst[i-1]*i)
+for i in range(n):
+    MAX=max(MAX,lst[i]*(i+1))
 print(MAX)
