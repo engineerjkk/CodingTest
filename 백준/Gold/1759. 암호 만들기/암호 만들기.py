6b495543673efeb9b87e7318c014dc04035e5a1a
JuneKoo Kang <engineerjkk@naver.com>
2024-07-08 22:44:17 +0900

    [Gold V] Title: 암호 만들기, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -2,12 +2,13 @@ import sys
 input = sys.stdin.readline
 from itertools import combinations
 L,C=map(int,input().split())
-moum=["a","e","i","o",'u']
 lst=list(map(str,input().split()))
 lst.sort()
+moum=["a","e","i","o","u"]
+
 for nCr in combinations(lst,L):
-    mo=0
     ja=0
+    mo=0
     for x in nCr:
         if x in moum:
             mo+=1
