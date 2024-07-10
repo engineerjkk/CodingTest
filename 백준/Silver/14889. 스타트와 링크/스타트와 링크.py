94af0fbe951acf5621a6bf09a7c36b7ea18ae0cb
JuneKoo Kang <engineerjkk@naver.com>
2024-07-10 23:36:21 +0900

    [Silver I] Title: 스타트와 링크, Time: 5060 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -5,28 +5,22 @@ n=int(input())
 space=[]
 for _ in range(n):
     space.append(list(map(int,input().split())))
-
 lst=[]
 for i in range(n):
     lst.append(i)
-
 MIN=sys.maxsize
 for start in combinations(lst,n//2):
     link=[]
     for i in lst:
         if i not in start:
             link.append(i)
-    sum_start=0
+    start_sum=0
     for i in start:
         for j in start:
-            sum_start+=space[i][j]
-    sum_link=0
+            start_sum+=space[i][j]
+    link_sum=0
     for i in link:
         for j in link:
-            sum_link+=space[i][j]
-
-
-    MIN=min(MIN,abs(sum_start-sum_link))
+            link_sum+=space[i][j]
+    MIN=min(MIN,abs(start_sum-link_sum))
 print(MIN)
-    
-    
