76ddac9e3b3b8a9512cce9dad9b22ba4e2b84ba7
JuneKoo Kang <engineerjkk@naver.com>
2024-07-10 23:53:36 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,12 +1,11 @@
 import sys
 input = sys.stdin.readline
 n=int(input())
-a=[[0]*10 for _ in range(n)]
-a[0]=[0,1,1,1,1,1,1,1,1,1]
-
+dp=[[0]*10 for _ in range(n)]
+dp[0]=[0,1,1,1,1,1,1,1,1,1]
 for i in range(1,n):
-    a[i][0]=a[i-1][1]
-    a[i][9]=a[i-1][8]
+    dp[i][0]=dp[i-1][1]
+    dp[i][9]=dp[i-1][8]
     for j in range(1,9):
-        a[i][j]=a[i-1][j-1]+a[i-1][j+1]
-print(sum(a[-1])%1000000000)
+        dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1]
+print(sum(dp[-1])%1000000000)
