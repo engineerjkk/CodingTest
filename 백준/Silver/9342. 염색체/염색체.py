741ba97b4e616beaa6734ada88b382cc0b91b540
JuneKoo Kang <engineerjkk@naver.com>
2024-07-11 00:01:05 +0900

    [Silver III] Title: 염색체, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9342. 염색체/염색체.py
+++ 백준/Silver/9342. 염색체/염색체.py
@@ -1,25 +1,23 @@
+check=["A","B","C","D","E","F"]
 T=int(input())
-check={"A","B","C","D","E","F"}
 for _ in range(T):
-    s=input()
-    if len(s)<3:
-        print("Good")
-        continue
-    if s[0]!="A":
+    s=str(input())
+    if s[0] != "A":
         if s[0] not in check:
             print("Good")
             continue
         s=s[1:]
-    if s[-1]!="C":
+    if s[-1] !="C":
         if s[-1] not in check:
             print("Good")
             continue
         s=s[:-1]
-    ans=s[0]
+    
+    tmp=s[0]
     for i in range(1,len(s)):
         if s[i]!=s[i-1]:
-            ans+=s[i]
-    if ans=="AFC":
+            tmp+=s[i]
+    if tmp=="AFC":
         print("Infected!")
     else:
         print("Good")
