d1704e81eff5958c7f187f04d5d8592d3c59bfe0
JuneKoo Kang <engineerjkk@naver.com>
2024-07-11 23:56:24 +0900

    [Silver III] Title: 블로그2, Time: 992 ms, Memory: 32100 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.py
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -1,10 +1,8 @@
-import sys
-input = sys.stdin.readline
 n=int(input())
 s=str(input())
 
 tmp=s[0]
-for i in range(1,n):
+for i in range(1,len(s)):
     if s[i]!=s[i-1]:
         tmp+=s[i]
 
@@ -13,8 +11,9 @@ blue=0
 for i in tmp:
     if i=="R":
         red+=1
-    else:
+    elif i=="B":
         blue+=1
+
 if red<blue:
     print(red+1)
 else:
