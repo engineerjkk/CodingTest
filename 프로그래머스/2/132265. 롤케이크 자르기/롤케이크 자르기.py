c8c68106d0539f8770761c006e224c8db1608a2f
JuneKoo Kang <engineerjkk@naver.com>
2024-08-06 13:30:00 +0900

    [level 2] Title: 롤케이크 자르기, Time: 792.29 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,14 +1,13 @@
 from collections import Counter
-
 def solution(topping):
-    dic = Counter(topping)
-    set_dic = set()
-    res = 0
+    dic=Counter(topping)
+    dic_set=set()
+    res=0
     for i in topping:
-        dic[i] -= 1
-        set_dic.add(i)
-        if dic[i] == 0:
+        dic[i]-=1
+        dic_set.add(i)
+        if dic[i]==0:
             dic.pop(i)
-        if len(dic) == len(set_dic):
-            res += 1
+        if len(dic) == len(dic_set):
+            res+=1
     return res
