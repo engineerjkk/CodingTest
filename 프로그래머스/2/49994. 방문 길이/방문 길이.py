fd2994ba0f0fe59e90aec0036b8937a716cb7868
JuneKoo Kang <engineerjkk@naver.com>
2024-08-07 15:33:45 +0900

    [level 2] Title: 방문 길이, Time: 0.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,12 +1,10 @@
 def solution(dirs):
     dic={"U":(1,0),"D":(-1,0),"L":(0,-1),"R":(0,1)}
-    x,y=0,0
+    x,y=5,5
     nxt_set=set()
     for d in dirs:
         dx,dy=dic[d]
         nx,ny=x+dx,y+dy
-        if -5<=nx<=5 and -5<=ny<=5:
-            nxt_set.add(((x,y),(nx,ny)))
-            nxt_set.add(((nx,ny),(x,y)))
-            x,y=nx,ny
+        nxt_set.add(((x,y),(nx,ny)))
+        nxt_set.add(((nx,ny),(x,y)))
     return len(nxt_set)//2
