8a26fc8aabdc2277182b4d3809e9337b57186eb0
JuneKoo Kang <engineerjkk@naver.com>
2024-08-09 23:27:53 +0900

    [level 2] Title: 택배상자, Time: 447.64 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -2,13 +2,12 @@ def solution(order):
     i=1
     idx=0
     stack=[]
-    while i <len(order)+1:
+    while i<len(order)+1:
         stack.append(i)
         while stack[-1]==order[idx]:
-            idx+=1
             stack.pop()
+            idx+=1
             if len(stack)==0:
                 break
         i+=1
-            
     return idx
