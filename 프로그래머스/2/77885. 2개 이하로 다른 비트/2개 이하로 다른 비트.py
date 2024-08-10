a59350b3af90afac6414f06200fc8860e05c2a32
JuneKoo Kang <engineerjkk@naver.com>
2024-08-10 13:10:07 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 112.47 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,10 +1,10 @@
 def solution(numbers):
-    answer=[]
+    answer = []
     for number in numbers:
         if number%2==0:
             answer.append(number+1)
         else:
             number='0'+bin(number)[2:]
-            number=number[:number.rindex('0')]+"10"+number[number.rindex('0')+2:]
+            number=number[:number.rindex('0')]+'10'+number[number.rindex('0')+2:]
             answer.append(int(number,2))
     return answer
