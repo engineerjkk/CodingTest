8c8547fa3215125c387d6ee742816e5971a6cb0b
JuneKoo Kang <engineerjkk@naver.com>
2024-08-10 13:17:50 +0900

    [level 2] Title: 소수 찾기, Time: 765.10 ms, Memory: 280 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,9 +1,9 @@
 from itertools import permutations
 def solution(numbers):
-    a=set()
+    s=set()
     for i in range(1,len(numbers)+1):
-        a|=set(map(int,map("".join,permutations(list(numbers),i))))
-    a-=set(range(2))
-    for i in range(2,int(max(a)**(0.5))+1):
-        a-=set(range(i*2,max(a)+1,i))
-    return len(a)
+        s|=set(map(int,map("".join,permutations(list(numbers),i))))
+    s-=set(range(2))
+    for i in range(2,int(max(s)**(0.5))+1):
+        s-=set(range(i*2,max(s)+1,i))
+    return len(s)
