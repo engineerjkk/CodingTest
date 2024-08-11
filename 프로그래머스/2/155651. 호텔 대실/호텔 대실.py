d0e8087fcee3155b80ca8ae4eb4c36536478dbad
JuneKoo Kang <engineerjkk@naver.com>
2024-08-12 07:21:07 +0900

    [level 2] Title: 호텔 대실, Time: 3.54 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,21 @@
+import heapq
+def solution(book_time):
+    answer = 1
+    book_time_ref=[]
+    for s,e in book_time:
+        start=int(s[:2])*60+int(s[3:])
+        end=int(e[:2])*60+int(e[3:])
+        book_time_ref.append((start,end))
+    book_time_ref.sort()
+    
+    heap=[]
+    for s,e in book_time_ref:
+        if not heap:
+            heapq.heappush(heap,e+10)
+            continue
+        if heap[0]<=s:
+            heapq.heappop(heap)
+        else:
+            answer+=1
+        heapq.heappush(heap,e+10)
+    return answer
