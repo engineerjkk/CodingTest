7eaa1f4902a37680c7563b42e1204e7e48c56306
JuneKoo Kang <engineerjkk@naver.com>
2024-08-12 06:47:26 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -11,5 +11,5 @@ def solution(storey):
             if (storey//10)%10>4:
                 storey+=10
             answer+=remainder
-        storey//=10            
+        storey//=10
     return answer
