59d4ce693f7e895843ee8f211f34c4add79abea3
JuneKoo Kang <engineerjkk@naver.com>
2024-08-12 06:57:52 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -6,11 +6,11 @@ def solution(n, wires):
         graph[b].append(a)
         
     def bfs(start):
-        visit=[False]*(n+1)
         queue=deque()
         queue.append(start)
+        visit=[False]*(n+1)
         visit[start]=True
-        cnt=1
+        cnt=0
         while queue:
             q=queue.popleft()
             for i in graph[q]:
@@ -19,8 +19,7 @@ def solution(n, wires):
                     queue.append(i)
                     cnt+=1
         return cnt
-        
-    answer=n    
+    answer=n
     for a,b in wires:
         graph[a].remove(b)
         graph[b].remove(a)
