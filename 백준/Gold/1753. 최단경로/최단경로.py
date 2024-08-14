61febb7d96e6cdd2815599a1fcd82913f4a2ea3d
JuneKoo Kang <engineerjkk@naver.com>
2024-08-14 20:24:04 +0900

    [Gold IV] Title: 최단경로, Time: 556 ms, Memory: 67356 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,28 +1,28 @@
 import sys
 import heapq
-input=sys.stdin.readline
+input = sys.stdin.readline
 V,E=map(int,input().split())
 K=int(input())
-lst=[[] for _ in range(V+1)]
+graph=[[] for _ in range(V+1)]
 for _ in range(E):
     u,v,w=map(int,input().split())
-    lst[u].append((v,w))
-
+    graph[u].append((v,w))
 distance=[1e9]*(V+1)
 
 def dijkstra(start):
-    pq=[]
-    heapq.heappush(pq,(0,start))
+    queue=[]
+    heapq.heappush(queue,(0,start))
     distance[start]=0
-    while pq:
-        dis,now=heapq.heappop(pq)
+
+    while queue:
+        dis,now=heapq.heappop(queue)
         if distance[now]<dis:
             continue
-        for v,w in lst[now]:
+        for v,w in graph[now]:
             cost=dis+w
             if cost<distance[v]:
                 distance[v]=cost
-                heapq.heappush(pq,(cost,v))
+                heapq.heappush(queue,(cost,v))
 
 
 dijkstra(K)
@@ -32,4 +32,4 @@ for i in range(1,V+1):
         print("INF")
     else:
         print(distance[i])
-    
+
