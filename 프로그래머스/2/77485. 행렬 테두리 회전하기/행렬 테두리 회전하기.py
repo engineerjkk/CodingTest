b7c8b4d99ce16b0b6b5925fa00984697698877bd
JuneKoo Kang <engineerjkk@naver.com>
2024-08-15 01:37:18 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 164.41 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,41 +1,40 @@
+from collections import deque
+
 def solution(rows, columns, queries):
-    answer = []
-    # 행렬 만들기
-    array = [[0 for col in range(columns)] for row in range(rows)]
-    t = 1
+    board, i = [], 1
     for row in range(rows):
-        for col in range(columns):
-            array[row][col] = t
-            t += 1
-    
-    for x1, y1, x2, y2 in queries:
-        tmp = array[x1 - 1][y1 - 1] # 가로로 옮겨질 값 저장
-        mini = tmp
-        
-        # 왼쪽 세로
-        for k in range(x1 - 1, x2 - 1):
-            test = array[k + 1][y1 - 1]
-            array[k][y1 - 1] = test
-            mini = min(mini, test)
-            
-        # 하단 가로
-        for k in range(y1 - 1, y2 - 1):
-            test = array[x2 - 1][k + 1]
-            array[x2 - 1][k] = test
-            mini = min(mini, test)
-            
-        # 오른쪽 세로
-        for k in range(x2 - 1, x1 - 1, -1):
-            test = array[k - 1][y2 - 1]
-            array[k][y2 - 1] = test
-            mini = min(mini, test)
-            
-        # 상단 가로
-        for k in range(y2 - 1, y1 - 1, -1):
-            test = array[x1 - 1][k - 1]
-            array[x1 - 1][k] = test
-            mini = min(mini, test)
-        
-        array[x1 - 1][y1] = tmp
-        answer.append(mini)
+        board.append(list(range(i, i + columns)))
+        i += columns
+
+    answer, queue = [], deque()
+    for query in queries:
+        r1, c1, r2, c2 = query
+        r1, c1, r2, c2 = r1-1, c1-1, r2-1, c2-1
+
+        # 위쪽 라인 처리
+        for cr in range(c1, c2 + 1):
+            queue.append(board[r1][cr])
+        # 오른쪽 라인 처리
+        for cc in range(r1 + 1, r2):
+            queue.append(board[cc][c2])
+        # 아래쪽 라인 처리
+        for cr in range(c1, c2 + 1)[::-1]:
+            queue.append(board[r2][cr])
+        # 왼쪽 라인 처리
+        for cc in range(r1 + 1, r2)[::-1]:
+            queue.append(board[cc][c1])
+
+        answer.append(min(queue))
+        queue.rotate(1)
+
+        # 큐에서 값을 빼면서 다시 보드에 값 채워넣기
+        for cr in range(c1, c2 + 1):
+            board[r1][cr] = queue.popleft()
+        for cc in range(r1 + 1, r2):
+            board[cc][c2] = queue.popleft()
+        for cr in range(c1, c2 + 1)[::-1]:
+            board[r2][cr] = queue.popleft()
+        for cc in range(r1 + 1, r2)[::-1]:
+            board[cc][c1] = queue.popleft()
+
     return answer
