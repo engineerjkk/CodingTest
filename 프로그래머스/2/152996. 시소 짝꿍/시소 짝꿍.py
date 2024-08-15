d51b801056ab5bf261e56dbb4122a058b6ec9cb3
JuneKoo Kang <engineerjkk@naver.com>
2024-08-15 13:08:17 +0900

    [level 2] Title: 시소 짝꿍, Time: 11.51 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -2,11 +2,11 @@ from collections import Counter
 def solution(weights):
     answer = 0
     counter=Counter(weights)
-    for key, value in counter.items():
+    for key,value in counter.items():
         if value>=2:
-            answer+=value*(value-1)//2
+            answer+=(value)*(value-1)//2
     weights=set(weights)
-    #2:3, 2:4, 3:4
+    #2:3,2:4,3:4
     for w in weights:
         if w*2/3 in weights:
             answer+=counter[w*2/3]*counter[w]
@@ -14,5 +14,4 @@ def solution(weights):
             answer+=counter[w*2/4]*counter[w]
         if w*3/4 in weights:
             answer+=counter[w*3/4]*counter[w]
-        
     return answer
