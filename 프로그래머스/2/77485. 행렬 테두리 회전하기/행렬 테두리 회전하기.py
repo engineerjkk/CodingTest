1148126625530a8848e45e07f13af040772df88d
JuneKoo Kang <engineerjkk@naver.com>
2024-08-15 13:48:07 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 169.68 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,40 +1,34 @@
 from collections import deque
-
 def solution(rows, columns, queries):
-    board, i = [], 1
-    for row in range(rows):
-        board.append(list(range(i, i + columns)))
-        i += columns
-
-    answer, queue = [], deque()
+    cnt=1
+    space=[[0]*columns for _ in range(rows)]
+    for i in range(rows):
+        for j in range(columns):
+            space[i][j]=cnt
+            cnt+=1
+    answer=[]
     for query in queries:
-        r1, c1, r2, c2 = query
-        r1, c1, r2, c2 = r1-1, c1-1, r2-1, c2-1
-
-        # 위쪽 라인 처리
-        for cr in range(c1, c2 + 1):
-            queue.append(board[r1][cr])
-        # 오른쪽 라인 처리
-        for cc in range(r1 + 1, r2):
-            queue.append(board[cc][c2])
-        # 아래쪽 라인 처리
-        for cr in range(c1, c2 + 1)[::-1]:
-            queue.append(board[r2][cr])
-        # 왼쪽 라인 처리
-        for cc in range(r1 + 1, r2)[::-1]:
-            queue.append(board[cc][c1])
-
-        answer.append(min(queue))
+        r1,c1,r2,c2=query
+        r1,c1,r2,c2=r1-1,c1-1,r2-1,c2-1
+        queue=deque()
+        for cr in range(c1,c2+1):
+            queue.append(space[r1][cr])
+        for cc in range(r1+1,r2):
+            queue.append(space[cc][c2])
+        for cr in range(c1,c2+1)[::-1]:
+            queue.append(space[r2][cr])
+        for cc in range(r1+1,r2)[::-1]:
+            queue.append(space[cc][c1])
+            
         queue.rotate(1)
-
-        # 큐에서 값을 빼면서 다시 보드에 값 채워넣기
-        for cr in range(c1, c2 + 1):
-            board[r1][cr] = queue.popleft()
-        for cc in range(r1 + 1, r2):
-            board[cc][c2] = queue.popleft()
-        for cr in range(c1, c2 + 1)[::-1]:
-            board[r2][cr] = queue.popleft()
-        for cc in range(r1 + 1, r2)[::-1]:
-            board[cc][c1] = queue.popleft()
-
+        answer.append(min(queue))
+        
+        for cr in range(c1,c2+1):
+            space[r1][cr]=queue.popleft()
+        for cc in range(r1+1,r2):
+            space[cc][c2]=queue.popleft()
+        for cr in range(c1,c2+1)[::-1]:
+            space[r2][cr]=queue.popleft()
+        for cc in range(r1+1,r2)[::-1]:
+            space[cc][c1]=queue.popleft()
     return answer
