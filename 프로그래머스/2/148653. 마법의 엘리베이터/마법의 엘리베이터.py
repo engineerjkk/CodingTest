f0bf61927ebd76d24310a5ca29acd46bb95de9ef
JuneKoo Kang <engineerjkk@naver.com>
2024-08-16 13:42:27 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,9 +1,9 @@
 def solution(storey):
-    answer = 0
+    answer=0
     while storey:
         remainder=storey%10
         if remainder>5:
-            answer+=10-remainder
+            answer+=(10-remainder)
             storey+=10
         elif remainder<5:
             answer+=remainder
@@ -11,5 +11,6 @@ def solution(storey):
             if (storey//10)%10>4:
                 storey+=10
             answer+=remainder
-        storey=storey//10
+        storey//=10
+            
     return answer
