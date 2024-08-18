5aba4a07c9c1b13cef3db90f00022c372ec5270a
JuneKoo Kang <engineerjkk@naver.com>
2024-08-18 13:59:50 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.38 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,6 +1,6 @@
 from itertools import permutations
 
-def operations(num1,num2,o):
+def operation(num1,num2,o):
     if o=='+':
         return str(int(num1)+int(num2))
     if o=='-':
@@ -19,22 +19,21 @@ def calculate(op,exp):
             array.append(i)
             tmp=""
     array.append(tmp)
-
     for o in op:
         stack=[]
         while len(array)!=0:
             tmp=array.pop(0)
             if tmp==o:
-                stack.append(operations(stack.pop(),array.pop(0),o))
+                stack.append(operation(stack.pop(),array.pop(0),o))
             else:
                 stack.append(tmp)
         array=stack
     return abs(int(array[0]))
+    
 def solution(expression):
-    answer = 0
+    answer=[]
     oper=['+','-','*']
-    lst=permutations(oper,3)
-    result=[]
-    for i in lst:
-        result.append(calculate(i,expression))
-    return max(result)
+    oper=permutations(oper,3)
+    for i in oper:
+        answer.append(calculate(i,expression))
+    return max(answer)
