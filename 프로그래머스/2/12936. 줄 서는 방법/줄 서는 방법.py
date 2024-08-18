31fa5053782c3a332b1ab959bbd96cf03810d7a5
JuneKoo Kang <engineerjkk@naver.com>
2024-08-18 14:47:52 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -0,0 +1,14 @@
+import math
+def solution(n, k):
+    answer = []
+    lst=[]
+    for i in range(1,n+1):
+        lst.append(i)
+    k-=1
+    while lst:
+        a=k//math.factorial(n-1)
+        answer.append(lst[a])
+        del lst[a]
+        k=k%math.factorial(n-1)
+        n-=1
+    return answer
