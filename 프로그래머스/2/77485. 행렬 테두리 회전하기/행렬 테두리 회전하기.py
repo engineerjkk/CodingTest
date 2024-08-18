26774f4cfb69c606eee72584d933b7d3fc0c5243
JuneKoo Kang <engineerjkk@naver.com>
2024-08-18 13:38:59 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 177.32 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,16 +1,16 @@
 from collections import deque
 def solution(rows, columns, queries):
     answer = []
-    cnt=1
     space=[[0]*columns for _ in range(rows)]
+    cnt=1
     for i in range(rows):
         for j in range(columns):
             space[i][j]=cnt
             cnt+=1
-    queue=deque()
     for query in queries:
         r1,c1,r2,c2=query
         r1,c1,r2,c2=r1-1,c1-1,r2-1,c2-1
+        queue=deque()
         for cr in range(c1,c2+1):
             queue.append(space[r1][cr])
         for cc in range(r1+1,r2):
@@ -19,8 +19,10 @@ def solution(rows, columns, queries):
             queue.append(space[r2][cr])
         for cc in range(r1+1,r2)[::-1]:
             queue.append(space[cc][c1])
+        
         queue.rotate(1)
         answer.append(min(queue))
+        
         for cr in range(c1,c2+1):
             space[r1][cr]=queue.popleft()
         for cc in range(r1+1,r2):
@@ -29,4 +31,5 @@ def solution(rows, columns, queries):
             space[r2][cr]=queue.popleft()
         for cc in range(r1+1,r2)[::-1]:
             space[cc][c1]=queue.popleft()
+    
     return answer
