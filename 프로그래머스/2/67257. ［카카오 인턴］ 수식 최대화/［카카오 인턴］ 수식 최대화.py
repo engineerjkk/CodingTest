c3724045e9f4e5719935dc528d7e4ef4ca72d917
JuneKoo Kang <engineerjkk@naver.com>
2024-08-19 13:06:26 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.32 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,39 +1,39 @@
 from itertools import permutations
-
-def operation(num1,num2,o):
-    if o=='+':
-        return str(int(num1)+int(num2))
-    if o=='-':
-        return str(int(num1)-int(num2))
-    if o=='*':
-        return str(int(num1)*int(num2))
-
-def calculate(op,exp):
-    tmp=""
-    array=[]
-    for i in exp:
-        if i.isdigit()==True:
-            tmp+=i
-        else:
-            array.append(tmp)
-            array.append(i)
-            tmp=""
-    array.append(tmp)
-    for o in op:
-        stack=[]
-        while len(array)!=0:
-            tmp=array.pop(0)
-            if tmp==o:
-                stack.append(operation(stack.pop(),array.pop(0),o))
-            else:
-                stack.append(tmp)
-        array=stack
-    return abs(int(array[0]))
-    
 def solution(expression):
-    answer=[]
+    
+    def operation(num1,num2,o):
+        if o=='+':
+            return str(int(num1)+int(num2))
+        if o=='-':
+            return str(int(num1)-int(num2))
+        if o=='*':
+            return str(int(num1)*int(num2))
+    
+    def calculate(op,exp):
+        tmp=""
+        array=[]
+        for i in exp:
+            if i.isdigit()==True:
+                tmp+=i
+            else:
+                array.append(tmp)
+                array.append(i)
+                tmp=""
+        array.append(tmp)
+        for o in op:
+            stack=[]
+            while len(array)!=0:
+                tmp=array.pop(0)
+                if o==tmp:
+                    stack.append(operation(stack.pop(),array.pop(0),o))
+                else:
+                    stack.append(tmp)
+            array=stack 
+        return abs(int(array[0]))
+    
     oper=['+','-','*']
     oper=permutations(oper,3)
+    result=[]
     for i in oper:
-        answer.append(calculate(i,expression))
-    return max(answer)
+        result.append(calculate(i,expression))
+    return max(result)
