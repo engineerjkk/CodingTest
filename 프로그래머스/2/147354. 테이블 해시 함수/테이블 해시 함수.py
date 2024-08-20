605003e856cfff5989ea8809b2e01e0e9f8388da
JuneKoo Kang <engineerjkk@naver.com>
2024-08-20 11:45:08 +0900

    [level 2] Title: 테이블 해시 함수, Time: 75.05 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,9 +1,9 @@
 def solution(data, col, row_begin, row_end):
-    data=sorted(data,key=lambda x:[x[col-1],-x[0]])
-    answer=0
+    answer = 0
+    data.sort(key=lambda x:[x[col-1],-x[0]])
     for i in range(row_begin,row_end+1):
         tmp=0
         for j in data[i-1]:
-            tmp+=(j%i)
+            tmp+=j%i
         answer^=tmp
     return answer
