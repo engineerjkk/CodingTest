8be98c971767c8bf989ac158e7c322b675813b69
JuneKoo Kang <engineerjkk@naver.com>
2024-08-20 11:36:06 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.50 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,39 +1,40 @@
 from itertools import permutations
-def solution(expression):
+
+def operation(num1,num2,o):
+    if o=="+":
+        return str(int(num1)+int(num2))
+    if o=="-":
+        return str(int(num1)-int(num2))
+    if o=="*":
+        return str(int(num1)*int(num2))
+
+def calculate(op,exp):
+    tmp=""
+    array=[]
+    for i in exp:
+        if i.isdigit()==True:
+            tmp+=i
+        else:
+            array.append(tmp)
+            array.append(i)
+            tmp=""
+    array.append(tmp)
     
-    def operation(num1,num2,o):
-        if o=='+':
-            return str(int(num1)+int(num2))
-        if o=='-':
-            return str(int(num1)-int(num2))
-        if o=='*':
-            return str(int(num1)*int(num2))
-    
-    def calculate(op,exp):
-        tmp=""
-        array=[]
-        for i in exp:
-            if i.isdigit()==True:
-                tmp+=i
+    for o in op:
+        stack=[]
+        while len(array)!=0:
+            tmp=array.pop(0)
+            if tmp==o:
+                stack.append(operation(stack.pop(),array.pop(0),o))
             else:
-                array.append(tmp)
-                array.append(i)
-                tmp=""
-        array.append(tmp)
-        for o in op:
-            stack=[]
-            while len(array)!=0:
-                tmp=array.pop(0)
-                if o==tmp:
-                    stack.append(operation(stack.pop(),array.pop(0),o))
-                else:
-                    stack.append(tmp)
-            array=stack 
-        return abs(int(array[0]))
-    
-    oper=['+','-','*']
-    oper=permutations(oper,3)
-    result=[]
-    for i in oper:
-        result.append(calculate(i,expression))
-    return max(result)
+                stack.append(tmp)
+        array=stack
+    return abs(int(array[0]))
+
+def solution(expression):
+    op=['+','-','*']
+    op=permutations(op,3)
+    answer=[]
+    for i in op:
+        answer.append(calculate(i,expression))
+    return max(answer)
