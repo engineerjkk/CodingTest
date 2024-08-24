18206b08e03c244e6972544530c6d2981aec5869
JuneKoo Kang <engineerjkk@naver.com>
2024-08-24 15:09:55 +0900

    [level 2] Title: 광물 캐기, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,33 +1,36 @@
 def solution(picks, minerals):
     answer = 0
-    sum=0
+    tmp=0
     for i in picks:
-        sum+=i
-    min_sum=sum*5
-    if len(minerals)>min_sum:
-        minerals=minerals[:min_sum]
-    new_minerals=[[0,0,0] for _ in range(min_sum//5+1)]
+        tmp+=i
+    min_minerals=tmp*5
+    if min_minerals<len(minerals):
+        minerals=minerals[:min_minerals]
+    
+    new_minerals=[[0,0,0] for _ in range(min_minerals//5+1)]
     for m in range(len(minerals)):
-        if minerals[m]=="diamond":
+        if minerals[m]=='diamond':
             new_minerals[m//5][0]+=1
-        elif minerals[m]=="iron":
+        elif minerals[m]=='iron':
             new_minerals[m//5][1]+=1
         else:
             new_minerals[m//5][2]+=1
     new_minerals.sort(key=lambda x:[x[0],x[1],x[2]],reverse=True)
-    for mineral in new_minerals:
-        dia,iron,stone=mineral
+    print(new_minerals)
+    
+    for m in new_minerals:
+        dia,iron,stone=m
         for i in range(len(picks)):
             if picks[i]>0 and i==0:
-                picks[i]-=1
                 answer+=dia+iron+stone
+                picks[i]-=1
                 break
             elif picks[i]>0 and i==1:
-                picks[i]-=1
                 answer+=dia*5+iron+stone
+                picks[i]-=1
                 break
             elif picks[i]>0 and i==2:
-                picks[i]-=1
                 answer+=dia*25+iron*5+stone
+                picks[i]-=1
                 break
     return answer
