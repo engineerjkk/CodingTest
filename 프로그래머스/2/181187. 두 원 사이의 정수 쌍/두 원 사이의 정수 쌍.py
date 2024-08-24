cc58abe31e7e129f350ff0390c42fe88fc8a371b
JuneKoo Kang <engineerjkk@naver.com>
2024-08-24 15:35:08 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 1038.29 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -1,10 +1,8 @@
 from math import sqrt
 def solution(r1, r2):
     answer=0
-    for x in range(0,r1):
-        y=int(sqrt(r2**2-x**2))-int(sqrt(r1**2-x**2-1))
-        answer+=y
-    for x in range(r1,r2):
-        y=int(sqrt(r2**2-x**2))
-        answer+=y
+    for i in range(0,r1):
+        answer+=int(sqrt(r2**2-i**2))-int(sqrt(r1**2-i**2-1))
+    for i in range(r1,r2+1):
+        answer+=int(sqrt(r2**2-i**2))
     return answer*4
