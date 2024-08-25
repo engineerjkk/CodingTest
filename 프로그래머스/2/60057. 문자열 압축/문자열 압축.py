0d7a37bd93e0740af07a1b9d9583a56529aa80a8
JuneKoo Kang <engineerjkk@naver.com>
2024-08-25 20:44:11 +0900

    [level 2] Title: 문자열 압축, Time: 2.57 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -15,12 +15,11 @@ def solution(s):
                     compressed.append(f"{cnt}{prev}")
                 else:
                     compressed.append(prev)
-                cnt=1    
+                cnt=1
                 prev=current
         if cnt>1:
             compressed.append(f"{cnt}{prev}")
         else:
             compressed.append(prev)
         min_length=min(min_length,len("".join(compressed)))
-                
     return min_length
