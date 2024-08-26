05e25c333da63cd69e5925fb0e5e469c9b48fb50
JuneKoo Kang <engineerjkk@naver.com>
2024-08-26 12:07:51 +0900

    [level 2] Title: 점 찍기, Time: 658.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -1,6 +1,7 @@
+from math import sqrt
 def solution(k, d):
     answer = 0
     for x in range(0,d+1,k):
-        max_y=int((d**2-x**2)**0.5)
+        max_y=int(sqrt(d**2-x**2))
         answer+=max_y//k+1
     return answer
