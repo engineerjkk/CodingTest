f03881a0847f65e233995970bf8d19bd0f1428fe
JuneKoo Kang <engineerjkk@naver.com>
2024-08-30 20:02:42 +0900

    [level 3] Title: 숫자 게임, Time: 40.99 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -1,10 +1,10 @@
 def solution(A, B):
     answer=0
+    cnt=0
     A.sort()
     B.sort()
-    cnt=0
     for i in range(len(A)):
         if A[cnt]<B[i]:
-            answer+=1
             cnt+=1
+            answer+=1
     return answer
