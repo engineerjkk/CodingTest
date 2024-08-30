7d0501316cb64c33ae66c0e5f2c08ba5ed9d10d1
JuneKoo Kang <engineerjkk@naver.com>
2024-08-30 23:45:10 +0900

    [level 3] Title: 가장 먼 노드, Time: 7487.23 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,26 +1,22 @@
 from collections import deque
 def solution(n, edge):
-    answer=0
+    answer = 0
     graph=[[] for _ in range(n+1)]
-    visited=[False]*(n+1)
-    
+    visit=[-1]*(n+1)
     for a,b in edge:
         graph[a].append(b)
         graph[b].append(a)
     
     queue=deque()
     queue.append(1)
-    visited[1]=1
+    visit[1]=0
     while queue:
         q=queue.popleft()
-        
         for i in graph[q]:
-            if visited[i]==False:
+            if visit[i]==-1:
+                visit[i]=visit[q]+1
                 queue.append(i)
-                visited[i]=visited[q]+1
-                
-    for dis in visited:
-        if dis ==max(visited):
+    for i in range(len(visit)):
+        if visit[i]==max(visit):
             answer+=1
-             
     return answer
