5594f5ba4a50a5bcfafe4dc559fd6d8d3f6b82a8
JuneKoo Kang <engineerjkk@naver.com>
2024-08-30 16:10:48 +0900

    [level 3] Title: 불량 사용자, Time: 401.46 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,24 +1,25 @@
 from itertools import permutations
-
-def check(per,ban):
-    if len(per)!=len(ban):
-        return False
-    for i in range(len(ban)):
-        if ban[i]=="*":
-            continue
-        if per[i]!=ban[i]:
-            return False
-    return True
-
 def solution(user_id, banned_id):
-    n=len(banned_id)
-    answer=[]
-    for permu in permutations(user_id,n):
+    answer = []
+    
+    def check(usr,ban):
+        if len(usr)!=len(ban):
+            return False
+        for i in range(len(usr)):
+            if ban[i]=='*':
+                continue
+            else:
+                if usr[i]!=ban[i]:
+                    return False
+        return True
+    
+    for permu in permutations(user_id,len(banned_id)):
         flag=True
-        for i in range(n):
+        for i in range(len(permu)):
             if not check(permu[i],banned_id[i]):
                 flag=False
         if flag:
             if set(permu) not in answer:
                 answer.append(set(permu))
+        
     return len(answer)
