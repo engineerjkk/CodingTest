d0277b283003af49b3a5245b17c6567d7bb19992
JuneKoo Kang <engineerjkk@naver.com>
2024-08-30 17:45:54 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,15 +1,14 @@
 def solution(n, costs):
-    answer=0
-    costs.sort(key=lambda x:x[2])
-    connect=set([costs[0][0]])
-    
-    while len(connect)!=n:
-        for i in costs:
-            if i[0] in connect and i[1] in connect:
+    answer = 0
+    costs.sort(key = lambda x: x[2]) 
+    link = set([costs[0][0]])
+    while len(link) != n:
+        for v in costs:
+            if v[0] in link and v[1] in link:
                 continue
-            elif i[0] in connect or i[1] in connect:
-                connect.update([i[0],i[1]])
-                answer+=i[2]
+            if v[0] in link or v[1] in link:
+                link.update([v[0], v[1]])
+                answer += v[2]
                 break
                 
     return answer
