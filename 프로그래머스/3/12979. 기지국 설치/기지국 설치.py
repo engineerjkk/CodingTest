54dc643629ab24cb33a452bfa5d4eac40a17b462
JuneKoo Kang <engineerjkk@naver.com>
2024-08-31 00:51:53 +0900

    [level 3] Title: 기지국 설치, Time: 5.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,14 +1,16 @@
-
 def solution(n, stations, w):
-    ans = 0
-    idx = 0
-    location = 1
-
-    while(location <= n) :
-        if(idx < len(stations) and location >= stations[idx]-w) :
-            location = stations[idx]+w+1
-            idx += 1
-        else :
-            location += 2*w+1
-            ans += 1
-    return ans
+    ans=0
+    idx=0
+    location=1
+    while location<=n:
+        # 기지국 범위에 있을 때
+        if idx<len(stations) and stations[idx]-w<=location<=stations[idx]+w:
+            location=stations[idx]+w+1
+            idx+=1
+        # 기지국 범위 밖일 때
+        else:
+            location+=w*2+1
+            ans+=1
+        
+        
+    return ans      
