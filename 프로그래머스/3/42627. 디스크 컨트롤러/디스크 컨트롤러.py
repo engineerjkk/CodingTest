ba2cedc4aa98193d899a95219cab9c19cf3c95e6
JuneKoo Kang <engineerjkk@naver.com>
2024-08-30 17:07:01 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 10.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,15 +1,14 @@
 import heapq
 def solution(jobs):
-    answer, now, i = 0, 0, 0
+    answer,i,now = 0,0,0
     start=-1
-    heap=[]
-    
+    pq=[]
     while i<len(jobs):
         for j in jobs:
             if start<j[0]<=now:
-                heapq.heappush(heap,[j[1],j[0]])
-        if len(heap)>0:
-            cur=heapq.heappop(heap)
+                heapq.heappush(pq,[j[1],j[0]])
+        if len(pq)>0:
+            cur=heapq.heappop(pq)
             start=now
             now+=cur[0]
             answer+=now-cur[1]
