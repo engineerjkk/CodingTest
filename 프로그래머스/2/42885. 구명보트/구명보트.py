5eef5a7aebd37e546b2e79f4bdc4b3f265c4a464
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 23:41:31 +0900

    [level 2] Title: 구명보트, Time: 9.09 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,18 +1,14 @@
 def solution(people, limit):
-    answer=0
-    people.sort()
+    answer = 0
     left=0
     right=len(people)-1
+    people.sort()
     while left<=right:
         if people[left]+people[right]<=limit:
-            answer+=1
             left+=1
             right-=1
+            answer+=1
         else:
             right-=1
             answer+=1
     return answer
-    
-
-
-        
