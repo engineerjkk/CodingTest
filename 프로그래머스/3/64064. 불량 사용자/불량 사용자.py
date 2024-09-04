2650c8fabf16bc21f5b7f4e681baf3f2c7292361
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 21:19:18 +0900

    [level 3] Title: 불량 사용자, Time: 376.67 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,16 +1,17 @@
 from itertools import permutations
-def check(per,ban):
-    if len(per)!=len(ban):
-        return False
-    for i in range(len(per)):
-        if ban[i]=='*':
-            continue
-        if per[i]!=ban[i]:
-            return False
-    return True
 def solution(user_id, banned_id):
-
     answer = []
+    
+    def check(usr,ban):
+        if len(usr)!=len(ban):
+            return False
+        for i in range(len(usr)):
+            if ban[i]=='*':
+                continue
+            if usr[i]!=ban[i]:
+                return False
+        return True
+    
     for permu in permutations(user_id,len(banned_id)):
         flag=True
         for i in range(len(permu)):
