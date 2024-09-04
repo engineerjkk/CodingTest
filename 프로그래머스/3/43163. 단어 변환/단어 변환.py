150340e0523ed9d3e2bcca1b0dd5dd33aadbe868
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 22:29:23 +0900

    [level 3] Title: 단어 변환, Time: 12.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -3,18 +3,17 @@ def solution(begin, target, words):
     answer = 0
     if target not in words:
         return 0
-    
     queue=deque()
     queue.append((begin,0))
     while queue:
-        now,step=queue.popleft()
-        if now==target:
+        q,step=queue.popleft()
+        if q==target:
             return step
         for word in words:
             cnt=0
-            for i in range(len(now)):
-                if now[i]!=word[i]:
+            for i in range(len(word)):
+                if q[i]!=word[i]:
                     cnt+=1
             if cnt==1:
                 queue.append((word,step+1))
-    return 0
+    return answer
