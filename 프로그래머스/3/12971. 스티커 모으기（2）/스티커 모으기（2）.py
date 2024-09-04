65c7aac8be4429408df52c34376371840abfec64
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 16:51:10 +0900

    [level 3] Title: 스티커 모으기(2), Time: 62.42 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,14 +1,18 @@
 def solution(sticker):
-    if len(sticker) <= 3:
+    
+    if len(sticker)<=3:
         return max(sticker)
     
+    #첫번째 카드를 선택한경우
     dp1=[0]*len(sticker)
     dp1[0],dp1[1]=sticker[0],max(sticker[0],sticker[1])
     for i in range(2,len(sticker)-1):
         dp1[i]=max(dp1[i-1],dp1[i-2]+sticker[i])
     
+    #첫번째 카드를 선택하지않은경우
     dp2=[0]*len(sticker)
     dp2[0],dp2[1]=0,sticker[1]
     for i in range(2,len(sticker)):
         dp2[i]=max(dp2[i-1],dp2[i-2]+sticker[i])
-    return max(dp1[len(sticker)-2],dp2[len(sticker)-1])
+
+    return max(dp1[-2],dp2[-1])
