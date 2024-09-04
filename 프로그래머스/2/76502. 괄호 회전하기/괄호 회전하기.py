7f2b39343f239d71d68b37c7c0a87486db396900
JuneKoo Kang <engineerjkk@naver.com>
2024-09-05 00:17:05 +0900

    [level 2] Title: 괄호 회전하기, Time: 344.16 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,32 +1,33 @@
 from collections import deque
-def solution(s):
-    
-    def check(s):
-        stack=[]
-        for i in s:
-            if len(stack)==0:
-                stack.append(i)
-            elif i=="}" and stack[-1]=="{":
+
+def check(s):
+    stack=[]
+    n=len(s)
+    for i in range(n):
+        if len(stack)==0:
+            stack.append(s[i])
+        else:
+            if stack[-1]=='[' and s[i]==']':
                 stack.pop()
-            elif i=="]" and stack[-1]=="[":
+            elif stack[-1]=='(' and s[i]==')':
                 stack.pop()
-            elif i==")" and stack[-1]=="(":
+            elif stack[-1]=='{' and s[i]=='}':
                 stack.pop()
             else:
-                stack.append(i)
-        if len(stack)==0:
-            return True
-        else:
-            return False
-    
-    n=len(s)
-    queue=deque(s)
-    answer=0
-    for _ in range(n):
-        if check(queue):
-            answer+=1
-        queue.rotate(-1)
-    return answer
-    
+                stack.append(s[i])
+    if len(stack)==0:
+        return True
+    else:
+        return False
+            
 
-    
+def solution(s):
+    answer = 0
+    n=len(s)
+    s=deque(s)
+    while n:
+        if check(s):
+            answer+=1
+        s.rotate(1)
+        n-=1
+    return answer
