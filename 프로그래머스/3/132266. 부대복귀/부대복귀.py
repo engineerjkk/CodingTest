531a936b4dc31351d61586b77b48708f6e624339
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 14:34:26 +0900

    [level 3] Title: 부대복귀, Time: 514.43 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -1,21 +1,20 @@
 from collections import deque
 def solution(n, roads, sources, destination):
     answer = []
-    graph=[[] for _ in range(n+1)]
-    visit=[-1]*(n+1)
+    graph=[[]for _ in range(n+1)]
     for a,b in roads:
         graph[a].append(b)
         graph[b].append(a)
-    
     queue=deque()
-    queue.append(destination)
+    queue.append((destination,0))
+    visit=[-1]*(n+1)
     visit[destination]=0
     while queue:
-        q=queue.popleft()
+        q,dis=queue.popleft()
         for i in graph[q]:
             if visit[i]==-1:
-                queue.append(i)
-                visit[i]=visit[q]+1
-    for i in sources:
-        answer.append(visit[i])
+                visit[i]=dis+1
+                queue.append((i,dis+1))
+    for s in sources:
+        answer.append(visit[s])
     return answer
