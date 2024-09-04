56baea2a477f53a9f59e34d45cee2709b28ee075
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 23:26:48 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,10 +1,9 @@
 def solution(n):
     answer=0
-    while n>0:
+    while n:
         if n%2==0:
-            n=n//2
+            n//=2
         else:
             n-=1
             answer+=1
     return answer
-        
