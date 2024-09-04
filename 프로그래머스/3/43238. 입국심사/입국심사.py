d993c6bd3b633ef2565928c266faf3343da682b5
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 17:58:20 +0900

    [level 3] Title: 입국심사, Time: 578.62 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,16 +1,14 @@
 def solution(n, times):
-    answer=0
-    max_value=max(times)
-    start,end=1,max_value*n
+    answer = 0
+    start=0
+    end=max(times)*n
     while start<=end:
         mid=(start+end)//2
         total=0
         for time in times:
             total+=mid//time
-            if total > n:
-                break
         
-        if total >=n:
+        if total>=n:
             end=mid-1
             answer=mid
         else:
