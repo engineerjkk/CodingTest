093dc1f5c3997fc04e33df6a9e620fe95516bdc4
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 23:47:40 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,6 +1,7 @@
 def solution(n):
+    answer = 0
     s=[0,1]
     for _ in range(1,n+1):
-        a=s[-2]+s[-1]
-        s.append(a)
+        a=s[-1]+s[-2]
+        s.append(a%1234567)
     return s[-1]%1234567
