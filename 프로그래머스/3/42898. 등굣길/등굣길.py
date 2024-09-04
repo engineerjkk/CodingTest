45ccb59e56218f8895923e8da1709d445e91a21f
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 14:53:06 +0900

    [level 3] Title: 등굣길, Time: 3.72 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,7 +1,7 @@
 def solution(m, n, puddles):
+    answer = 0
     space=[[0]*(m+1) for _ in range(n+1)]
     space[1][1]=1
-    
     for i in range(1,n+1):
         for j in range(1,m+1):
             if i==1 and j==1:
@@ -11,5 +11,4 @@ def solution(m, n, puddles):
             else:
                 space[i][j]=space[i-1][j]+space[i][j-1]
                 
-
     return space[n][m]%1000000007
