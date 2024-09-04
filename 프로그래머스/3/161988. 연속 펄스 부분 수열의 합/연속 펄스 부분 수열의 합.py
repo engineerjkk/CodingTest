164f6bd3c4980c93ac41f62a577cfa4803556c16
JuneKoo Kang <engineerjkk@naver.com>
2024-09-04 14:17:44 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 344.83 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,16 +1,14 @@
-def get_max(sequence,pulse):
-    sum_up=0
-    ret=-1
-    for now in sequence:
-        if sum_up<=0:
-            sum_up=now*pulse
-        else:
-            sum_up+=now*pulse
-        ret=max(ret,sum_up)
-        pulse*=-1
-    return ret
-            
-
 def solution(sequence):
-    answer=max(get_max(sequence,1),get_max(sequence,-1))
+    def check(seq,pulse):
+        answer = -1
+        tmp=0
+        for i in seq:
+            if tmp<=0:
+                tmp=i*pulse
+            else:
+                tmp+=i*pulse
+            pulse*=-1
+            answer=max(answer,tmp)
+        return answer
+    answer=max(check(sequence,-1),check(sequence,1))
     return answer
