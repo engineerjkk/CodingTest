c71532b30d8fce07b94013b5f577ecb2a368b02b
JuneKoo Kang <engineerjkk@naver.com>
2024-09-05 15:16:39 +0900

    [level 2] Title: [1차] 캐시, Time: 84.84 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,19 +1,15 @@
 from collections import deque
 def solution(cacheSize, cities):
+    answer = 0
     queue=deque(maxlen=cacheSize)
     time=0
-    for i in cities:
-        i=i.lower()
-        if i in queue:
-            queue.remove(i)
-            queue.append(i)
+    for city in cities:
+        city=city.lower()
+        if city in queue:
+            queue.remove(city)
+            queue.append(city)
             time+=1
         else:
-            queue.append(i)
+            queue.append(city)
             time+=5
     return time
-
-    
-
-        
-
