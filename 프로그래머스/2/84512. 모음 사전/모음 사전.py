65e1b8854862fbc6fe0b844d726ae159fe1f13ae
JuneKoo Kang <engineerjkk@naver.com>
2024-09-05 18:30:18 +0900

    [level 2] Title: 모음 사전, Time: 1.80 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,9 +1,10 @@
 from itertools import product
 def solution(word):
-    lst=["A","E","I","O","U"]
-    answer=[]
+    lst=['A','E','I','O','U']
+    tmp=[]
     for i in range(1,6):
         for pro in product(lst,repeat=i):
-            answer.append("".join(pro))
-    answer.sort()
-    return answer.index(word)+1
+            tmp.append(''.join(pro))
+    tmp.sort()
+    answer=tmp.index(word)+1
+    return answer
