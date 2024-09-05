97d3a8a9183527e3003c8785c3c7aae6345f69fb
JuneKoo Kang <engineerjkk@naver.com>
2024-09-05 17:07:41 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 110.78 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,38 +1,32 @@
-def transform(num,k):
-    numOver={10:"A",11:"B",12:"C",13:"D",14:"E",15:"F"}
-    ret=""
-    while num>0:
-        remainder=num%k
-        if remainder>=10:
-            ret+=numOver[remainder]
-        else:
-            ret+=str(remainder)
-        num=num//k
-    return ret[::-1]
-
-def check_prime(num):
-    if num==2 or num==3:
-        return True
-    if num%2==0 or num<2:
-        return False
-    for i in range(3,int(num**(0.5))+1):
-        if num%i==0:
-            return False
-    return True
-    
-            
-
 def solution(n, k):
-    k_num=transform(n,k)
-    k_num=k_num.split("0")
-    tmp=[]
-    ans=0
-    for s in k_num:
-        if s=="":
+    def transform(num,k):
+        dic={10:'A',11:'B',12:'C',13:'D',14:'E',15:'F'}
+        tmp=''
+        while num:
+            remainder=num%k
+            # if remainder>=10:
+            #     tmp+=dic[remainder]
+            # else:
+            tmp+=str(remainder)
+            num//=k
+        return tmp[::-1]
+    
+    def checkPrime(n):
+        if n==2 or n==3:
+            return True
+        if n%2==0 or n<2:
+            return False
+        for i in range(3,int(n**0.5)+1):
+            if n%i==0:
+                return False
+        return True
+    
+    num=transform(n,k)
+    num=num.split('0')
+    answer = 0
+    for n in num:
+        if n=='':
             continue
-        tmp.append(list(map(int,s.split(","))))
-    for i in range(len(tmp)):
-        for j in tmp[i]:
-            if check_prime(int(j)):
-                ans+=1
-    return ans
+        if checkPrime(int(n)):
+            answer+=1
+    return answer
