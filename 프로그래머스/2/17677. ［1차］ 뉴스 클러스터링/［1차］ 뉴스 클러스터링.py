c184cea771476e38f2a0c6b713ab08e5ec8da9e4
JuneKoo Kang <engineerjkk@naver.com>
2024-09-05 16:41:10 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.62 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,22 +1,19 @@
-import copy
 def solution(str1, str2):
-    answer=0
-    def transform(string):
+    answer = 0
+    def transform(cluster):
         ret=[]
-        for i in range(1,len(string)):
-            a=string[i-1]+string[i]
+        for i in range(1,len(cluster)):
+            a=cluster[i-1]+cluster[i]
             if a.isalpha():
                 ret.append(a)
         return ret
-    
-    total1,total2=transform(str1.lower()),transform(str2.lower())
+    str1,str2=transform(str1.lower()),transform(str2.lower())
     intersection=[]
-    for t1 in total1:
-        if t1 in total2:
-            total2.remove(t1)
-            intersection.append(t1)
-    
-    union=list(total1+total2)
+    for s in str1:
+        if s in str2:
+            str2.remove(s)
+            intersection.append(s)
+    union=str1+str2
     if len(union)==0:
         return 65536
     answer=int((len(intersection)/len(union))*65536)
