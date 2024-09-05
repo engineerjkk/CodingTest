9e3d29d568c6577e76ea909f1e9db9ac1df15883
JuneKoo Kang <engineerjkk@naver.com>
2024-09-05 23:30:11 +0900

    [level 2] Title: 2 x n 타일링, Time: 9.28 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,6 +1,7 @@
 def solution(n):
+    answer = 0
     s=[0,1,2]
     for i in range(3,n+1):
-        a=s[-1]+s[-2]
+        a=s[-2]+s[-1]
         s.append(a%1000000007)
-    return s[-1]%1000000007
+    return s[-1]
