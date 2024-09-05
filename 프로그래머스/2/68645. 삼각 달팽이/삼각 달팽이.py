5420be5e3086bfa37c50b2596b78d6d5a7f7c759
JuneKoo Kang <engineerjkk@naver.com>
2024-09-06 00:56:04 +0900

    [level 2] Title: 삼각 달팽이, Time: 118.51 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,7 +1,8 @@
 from itertools import chain
 def solution(n):
-    board=[[None]*i for i in range(1,n+1)]
-    [row,col,cnt]=[-1,0,1]
+    row,col,cnt=-1,0,1
+    board=[[0]*i for i in range(1,n+1)]
+    
     for i in range(n):
         for _ in range(n-i):
             if i%3==0:
