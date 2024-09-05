3abdda4b19443c9b3703e3fc65c6bc06eb2b419e
JuneKoo Kang <engineerjkk@naver.com>
2024-09-05 18:21:46 +0900

    [level 2] Title: [3차] n진수 게임, Time: 371.38 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,26 +1,27 @@
-def transform(num,n):
-    if num==0:
-        return "0"
-    ret=""
-    numOver={10:"A",11:"B",12:"C",13:"D",14:"E",15:"F"}
+def transform(num,i):
+    numOver={10:'A',11:'B',12:'C',13:'D',14:'E',15:'F'}
+    ret=''
     while num>0:
-        remainder=num%n
+        remainder=num%i
         if remainder>=10:
             ret+=numOver[remainder]
         else:
             ret+=str(remainder)
-        num=num//n
+        num=num//i
     return ret[::-1]
 
 def solution(n, t, m, p):
+    
     answer = ''
-    result=""
-    for i in range(t*m):
-        result+=transform(i,n)
+    tmp='0'
+    for i in range(1,t*m):
+        tmp+=transform(i,n)
+    
     p-=1
-    while True:
+    while p<len(tmp):
         if len(answer)==t:
-            break
-        answer+=result[p]
+            return answer
+        answer+=tmp[p]
         p+=m
+        
     return answer
