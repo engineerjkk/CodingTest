b80c241fe7939a1140afdc326cd54c94fe3deb1b
JuneKoo Kang <engineerjkk@naver.com>
2024-09-06 16:12:52 +0900

    [level 2] Title: 우박수열 정적분, Time: 9.61 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
@@ -3,20 +3,22 @@ def solution(k, ranges):
     lst=[k]
     while k>1:
         if k%2==0:
-            k=k//2
+            k//=2
             lst.append(k)
         else:
-            k=k*3+1
+            k*=3
+            k+=1
             lst.append(k)
     integral=[]
-    for i in range(len(lst)-1):
-        integral.append((lst[i]+lst[i+1])/2)
+    for i in range(1,len(lst)):
+        integral.append((lst[i-1]+lst[i])/2)
+    
     for s,e in ranges:
         e=len(integral)+e
-        if s==e:
-            answer.append(0)
-        elif e<s:
-            answer.append(-1)
+        if e<s:
+            answer.append(-1.0)
+        elif e==s:
+            answer.append(0.0)
         else:
             answer.append(sum(integral[s:e]))
     return answer
