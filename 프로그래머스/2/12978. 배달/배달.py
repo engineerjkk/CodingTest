6fa21db849c221260216d1a4a4f4680926c754fc
JuneKoo Kang <engineerjkk@naver.com>
2024-09-06 13:27:59 +0900

    [level 2] Title: 배달, Time: 3.26 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,26 +1,24 @@
-import heapq
+from collections import deque
 def solution(N, road, K):
-    answer = 0
-    graph=[[]for _ in range(N+1)]
-    for u,v,w in road:
-        graph[u].append((v,w))
-        graph[v].append((u,w))
-    distance=[1e9]*(N+1)
+    graph=[[] for _ in range(N+1)]
+    for a,b,c in road:
+        graph[a].append((b,c))
+        graph[b].append((a,c))
+    visit=[1e9]*(N+1)
     def dijkstra(start):
-        pq=[]
-        heapq.heappush(pq,(0,start))
-        distance[start]=0
-        while pq:
-            dis,now=heapq.heappop(pq)
-            if distance[now]<dis:
-                continue
+        queue=deque()
+        queue.append((start,0))
+        visit[start]=0
+        while queue:
+            now,dis=queue.popleft()
             for v,w in graph[now]:
                 cost=dis+w
-                if cost<distance[v]:
-                    distance[v]=cost
-                    heapq.heappush(pq,(cost,v))
+                if cost<visit[v]:
+                    visit[v]=cost
+                    queue.append((v,cost))
     dijkstra(1)
+    answer=0
     for i in range(1,N+1):
-        if distance[i]<=K:
+        if visit[i]<=K:
             answer+=1
     return answer
