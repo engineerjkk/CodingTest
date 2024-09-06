a7c7b9e9c0dcc2b409a960d4fb0e0e55bb7bd9b1
JuneKoo Kang <engineerjkk@naver.com>
2024-09-06 16:52:31 +0900

    [level 2] Title: 과제 진행하기, Time: 72.00 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,3 +1,24 @@
+from collections import deque
 def solution(plans):
     answer = []
+    queue=deque()
+    lst=[]
+    for plan in plans:
+        study,start,playtime=plan
+        start=int(start[:2])*60+int(start[3:])
+        playtime=int(playtime)
+        lst.append((study,start,playtime))
+    lst.sort(key=lambda x:x[1])
+    queue=deque(lst)
+    lst=[]
+    while queue:
+        study,start,playtime=queue.popleft()
+        for i in range(len(lst)):
+            n,t=lst[i]
+            if start<t:
+                lst[i][1]+=playtime
+        lst.append([study,start+playtime])
+    lst.sort(key=lambda x:x[1])
+    for n,t in lst:
+        answer.append(n)
     return answer
