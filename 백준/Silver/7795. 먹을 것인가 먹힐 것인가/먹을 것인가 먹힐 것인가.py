6acba10995afbe0bb75f083e55e0c38fbcfe2aa3
JuneKoo Kang <engineerjkk@naver.com>
2024-09-06 22:12:48 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 124 ms, Memory: 34488 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,23 +1,25 @@
 import sys
 input = sys.stdin.readline
-T=int(input())
-for _ in range(T):
+t=int(input())
+answer=[]
+for _ in range(t):
     n,m=map(int,input().split())
-    lst_n=list(map(int,input().split()))
-    lst_m=list(map(int,input().split()))
-    lst_n.sort()
-    lst_m.sort()
-    sub=0
+    A=list(map(int,input().split()))
+    B=list(map(int,input().split()))
+    A.sort()
+    B.sort()
     main=0
-    cnt=0
+    sub=0
+    ans=0
     while main<n:
         if sub==m:
-            cnt+=sub
+            ans+=sub
+            main+=1
+        elif A[main]<=B[sub]:
+            ans+=sub
             main+=1
         else:
-            if lst_n[main]<=lst_m[sub]:
-                cnt+=sub
-                main+=1
-            else:
-                sub+=1
-    print(cnt)
+            sub+=1
+    answer.append(ans)
+for i in answer:
+    print(i)
