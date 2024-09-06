85a11d1e3238666b70c8f900f1a874d14bb80687
JuneKoo Kang <engineerjkk@naver.com>
2024-09-06 13:52:37 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 176.90 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -10,26 +10,27 @@ def solution(rows, columns, queries):
     for query in queries:
         r1,c1,r2,c2=query
         r1,c1,r2,c2=r1-1,c1-1,r2-1,c2-1
+
         queue=deque()
-        for cr in range(c1,c2+1):
-            queue.append(space[r1][cr])
-        for cc in range(r1+1,r2):
-            queue.append(space[cc][c2])
-        for cr in range(c1,c2+1)[::-1]:
-            queue.append(space[r2][cr])
-        for cc in range(r1+1,r2)[::-1]:
-            queue.append(space[cc][c1])
+        for r in range(c1,c2+1):
+            queue.append(space[r1][r])
+        for c in range(r1+1,r2):
+            queue.append(space[c][c2])
+        for r in range(c1,c2+1)[::-1]:
+            queue.append(space[r2][r])
+        for c in range(r1+1,r2)[::-1]:
+            queue.append(space[c][c1])
         
         queue.rotate(1)
         answer.append(min(queue))
-        
-        for cr in range(c1,c2+1):
-            space[r1][cr]=queue.popleft()
-        for cc in range(r1+1,r2):
-            space[cc][c2]=queue.popleft()
-        for cr in range(c1,c2+1)[::-1]:
-            space[r2][cr]=queue.popleft()
-        for cc in range(r1+1,r2)[::-1]:
-            space[cc][c1]=queue.popleft()
-    
+
+        for r in range(c1,c2+1):
+            space[r1][r]=queue.popleft()
+        for c in range(r1+1,r2):
+            space[c][c2]=queue.popleft()
+        for r in range(c1,c2+1)[::-1]:
+            space[r2][r]=queue.popleft()
+        for c in range(r1+1,r2)[::-1]:
+            space[c][c1]=queue.popleft()
+
     return answer
