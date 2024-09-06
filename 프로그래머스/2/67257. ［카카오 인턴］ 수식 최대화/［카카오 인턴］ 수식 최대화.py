4dab95428f5b7cb91388319278ab50afdc282523
JuneKoo Kang <engineerjkk@naver.com>
2024-09-06 14:18:38 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.52 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,41 +1,38 @@
 from itertools import permutations
-
-def operation(num1,num2,o):
-    if o=='+':
-        return str(int(num1)+int(num2))
-    if o=='-':
-        return str(int(num1)-int(num2))
-    if o=='*':
-        return str(int(num1)*int(num2))
-
-def calculate(op,exp):
-    tmp=""
-    array=[]
-    for i in exp:
-        if i.isdigit()==True:
-            tmp+=i
-        else:
-            array.append(tmp)
-            array.append(i)
-            tmp=""
-    array.append(tmp)
-    for o in op:
-        stack=[]
-        while len(array)!=0:
-            tmp=array.pop(0)
-            if tmp==o:
-                stack.append(operation(stack.pop(),array.pop(0),o))
-            else:
-                stack.append(tmp)
-        array=stack
-    return abs(int(array[0]))
-            
-
 def solution(expression):
+    
+    def operations(num1,num2,o):
+        if o=='+':
+            return str(int(num1)+int(num2))
+        if o=='-':
+            return str(int(num1)-int(num2))
+        if o=='*':
+            return str(int(num1)*int(num2))
+    
+    def calculate(op,exp):
+        tmp=''
+        array=[]
+        for i in exp:
+            if i.isdigit():
+                tmp+=i
+            else:
+                array.append(tmp)
+                array.append(i)
+                tmp=''
+        array.append(tmp)
+        for o in op:
+            stack=[]
+            while len(array)!=0:
+                tmp=array.pop(0)
+                if tmp==o:
+                    stack.append(operations(stack.pop(),array.pop(0),o))
+                else:
+                    stack.append(tmp)
+            array=stack
+        return abs(int(array[0]))
+    
     answer = 0
-    oper=['+','-','*']
-    oper=list(permutations(oper,3))
-    result=[]
-    for i in oper:
-        result.append(calculate(i,expression))
-    return max(result)
+    operation=['+','-','*']
+    for op in list(permutations(operation,3)):
+        answer=max(answer,calculate(op,expression))
+    return answer
