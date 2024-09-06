7444c40a8b5a8af97eec41735119255327a56ebd
JuneKoo Kang <engineerjkk@naver.com>
2024-09-06 15:17:05 +0900

    [level 2] Title: 리코쳇 로봇, Time: 11.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,25 +1,26 @@
 from collections import deque
 def solution(board):
     answer = 0
+    flag=False
     n=len(board)
     m=len(board[0])
     queue=deque()
-    distance=[[1e9]*m for _ in range(n)]
-    flag=False
+    visit=[[1e9]*m for _ in range(n)]
     for i in range(n):
         for j in range(m):
             if board[i][j]=='R':
                 queue.append((i,j,0))
-                distance[i][j]=0
+                visit[i][j]=0
                 flag=True
                 break
         if flag:
             break
+    dr=[0,1,0,-1]
+    dc=[1,0,-1,0]
+    
     def in_range(r,c):
         return -1<r<n and -1<c<m
-    dr=[-1,0,1,0]
-    dc=[0,1,0,-1]
-    
+    cnt=1
     while queue:
         r,c,dis=queue.popleft()
         if board[r][c]=='G':
@@ -30,8 +31,10 @@ def solution(board):
             while in_range(nr+dr[i],nc+dc[i]) and board[nr+dr[i]][nc+dc[i]]!='D':
                 nr+=dr[i]
                 nc+=dc[i]
-            if dis+1<distance[nr][nc]:
-                distance[nr][nc]=dis+1
+            if dis+1<visit[nr][nc]:
+                visit[nr][nc]=dis+1
                 queue.append((nr,nc,dis+1))
-                
+
+
+    
     return -1
