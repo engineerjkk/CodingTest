34efd609791fcbd8f3ea01301636581b5880ad9e
JuneKoo Kang <engineerjkk@naver.com>
2024-09-06 13:38:33 +0900

    [level 2] Title: [3차] 방금그곡, Time: 1.92 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -1,15 +1,17 @@
-def shap_to_lower(s):
-    s=s.replace("A#",'a').replace("B#",'b').replace("C#",'c').replace("D#",'d').replace("E#",'e').replace("F#",'f').replace("G#",'g')
-    return s
+def shap_to_lower(m):
+    m=m.replace('A#','a').replace('B#','b').replace('C#','c').replace('D#','d').replace('E#','e').replace('F#','f').replace('G#','g')
+    return m
 
 def solution(m, musicinfos):
-    answer = [0,'(None)']
+    answer = [0,"(None)"]
     m=shap_to_lower(m)
     for musicinfo in musicinfos:
         s,e,name,music=musicinfo.split(",")
+        s=int(s[:2])*60+int(s[3:])
+        e=int(e[:2])*60+int(e[3:])
         music=shap_to_lower(music)
-        time_length=(int(e[:2])-int(s[:2]))*60+(int(e[3:])-int(s[3:]))
-        full_name=music*(time_length//len(music))+music[:time_length%len(music)]
-        if m in full_name and time_length>answer[0]:
+        time_length=e-s
+        full_music=music*(time_length//len(music))+music[:time_length%len(music)]
+        if m in full_music and answer[0]<time_length:
             answer=[time_length,name]
     return answer[1]
