3bfa3796d6cfba4a0babddc9e0088f564aacf1d9
JuneKoo Kang <engineerjkk@naver.com>
2024-10-11 22:50:47 +0900

    [level 2] Title: 피보나치 수, Time: 32.86 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,6 @@
 def solution(n):
-    answer = 0
     s=[0,1]
     for i in range(2,n+1):
-        a=s[-1]+s[-2]
-        s.append(a%1234567)
+        c=s[i-1]+s[i-2]
+        s.append(c%1234567)
     return s[-1]
