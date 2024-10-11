a7c39c6a9baecbb5499c190a19d38edcfd413415
JuneKoo Kang <engineerjkk@naver.com>
2024-10-12 00:37:50 +0900

    [level 3] Title: 단속카메라, Time: 0.88 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,10 +1,9 @@
 def solution(routes):
-    answer = 0
+    answer=0
     key=-30001
     routes.sort(key=lambda x:x[1])
     for s,e in routes:
         if key<s:
             key=e
             answer+=1
-        
     return answer
