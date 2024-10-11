d67d473990face95cdd76566ddf034cf9be4c843
JuneKoo Kang <engineerjkk@naver.com>
2024-10-12 00:27:59 +0900

    [level 3] Title: 야근 지수, Time: 397.40 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,14 +1,15 @@
 import heapq
 def solution(n, works):
-    answer = 0
     if sum(works)<=n:
         return 0
     pq=[]
     for i in range(len(works)):
         heapq.heappush(pq,-works[i])
+    
     while n:
         heapq.heappush(pq,heapq.heappop(pq)+1)
         n-=1
+    answer=0
     while pq:
         answer+=heapq.heappop(pq)**2
     return answer
