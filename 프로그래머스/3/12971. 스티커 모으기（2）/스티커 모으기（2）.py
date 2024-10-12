2c967c3dcba688cf0f25bb0296a30c50ab5a017c
JuneKoo Kang <engineerjkk@naver.com>
2024-10-12 10:25:48 +0900

    [level 3] Title: 스티커 모으기(2), Time: 64.73 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,13 +1,14 @@
 def solution(sticker):
     if len(sticker)<=3:
         return max(sticker)
-    #첫번째 스티커를 붙인경우
+    
+    #첫번째 스티커를 붙인 경우
     dp1=[0]*len(sticker)
     dp1[0],dp1[1]=sticker[0],max(sticker[0],sticker[1])
     for i in range(2,len(sticker)-1):
         dp1[i]=max(dp1[i-1],dp1[i-2]+sticker[i])
     
-    #첫분째 스티커를 붙이지 않은경우
+    #첫번째 스티커를 안붙인 경우
     dp2=[0]*len(sticker)
     dp2[0],dp2[1]=0,sticker[1]
     for i in range(2,len(sticker)):
