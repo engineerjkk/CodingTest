cee922f0ffe40192ade5c833b3766a18e66d287e
JuneKoo Kang <engineerjkk@naver.com>
2024-12-21 15:08:55 +0900

    [Gold V] Title: 빗물, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -2,13 +2,22 @@ import sys
 input = sys.stdin.readline
 
 def solution(h,w,blocks):
+    if w<=2:
+        return 0
     total_water=0
-    for i in range(1,w-1):
-        left_max=max(blocks[:i])
-        right_max=max(blocks[i+1:])
-        water_level=min(left_max,right_max)
-        if water_level>blocks[i]:
-            total_water+=water_level-blocks[i]
+    left,right=0,w-1
+    left_max,right_max=0,0
+    while left<right:
+        left_max=max(left_max,blocks[left])
+        right_max=max(right_max,blocks[right])
+        if left_max<=right_max:
+            water=left_max-blocks[left]
+            left+=1
+        else:
+            water=right_max-blocks[right]
+            right-=1
+        if water>0:
+            total_water+=water
     return total_water
 
 
