6e7f51b57bb9c462018dcc0cc3e159fc1016ee16
JuneKoo Kang <engineerjkk@naver.com>
2024-12-21 16:54:05 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 1064 ms, Memory: 81536 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -2,8 +2,8 @@ import sys
 input = sys.stdin.readline
 n=int(input())
 
-stack=[]
 score=0
+stack=[]
 for _ in range(n):
     info=list(map(int,input().split()))
     if info[0]==0:
@@ -13,9 +13,9 @@ for _ in range(n):
                 score+=a
             else:
                 stack.append((a,t-1))
+    
     if info[0]==1:
-        a=info[1]
-        t=info[2]
+        a,t=info[1],info[2]
         if t==1:
             score+=a
         else:
