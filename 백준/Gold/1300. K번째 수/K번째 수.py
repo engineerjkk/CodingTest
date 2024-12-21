c2cc3ce7d47b2cc7c728e23fe2111baef4d71d76
JuneKoo Kang <engineerjkk@naver.com>
2024-12-21 16:01:28 +0900

    [Gold I] Title: K번째 수, Time: 356 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,26 +1,27 @@
-def count_less_or_equal(x, n):
-    count = 0
-    for i in range(1, n + 1):
-        count += min(x // i, n)
+import sys
+input = sys.stdin.readline
+
+def Count(x,n):
+    count=0
+    for i in range(1,n+1):
+        count+=min(x//i,n)
     return count
 
-def solution(n, k):
-    left, right = 1, k  
-    
-    while left <= right:
-        mid = (left + right) // 2
-        
-        count = count_less_or_equal(mid, n)
-        
-        if count < k:  
-            left = mid + 1
-        else:  
-            answer = mid
-            right = mid - 1
-            
+def solution(n,k):
+    left,right=1,k
+    while left<=right:
+        mid=(left+right)//2
+        count=Count(mid,n)
+
+        if count<k:
+            left=mid+1
+        else:
+            answer=mid
+            right=mid-1
     return answer
 
-n = int(input())
-k = int(input())
+        
 
-print(solution(n, k))
+n=int(input())
+k=int(input())
+print(solution(n,k))
