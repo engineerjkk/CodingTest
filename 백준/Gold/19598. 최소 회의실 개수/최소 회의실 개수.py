d9f4e99f873fde31f52e0e7d56a5f013ca73e371
JuneKoo Kang <engineerjkk@naver.com>
2024-12-21 17:22:59 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 284 ms, Memory: 48580 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -1,24 +1,22 @@
 import sys
 input = sys.stdin.readline
 import heapq
+
 n=int(input())
 courses=[]
-
 for _ in range(n):
     start,end=map(int,input().split())
     courses.append((start,end))
 
 courses.sort()
-
 pq=[]
 heapq.heappush(pq,courses[0][1])
-count=1
-
+room=1
 for i in range(1,n):
     earliest=pq[0]
     if earliest<=courses[i][0]:
         heapq.heappop(pq)
     else:
-        count+=1
+        room+=1
     heapq.heappush(pq,courses[i][1])
-print(count)
+print(room)
