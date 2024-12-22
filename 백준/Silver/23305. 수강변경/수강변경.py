9bd8c30bc35181a79f0963efee3bb55dde0c60e9
JuneKoo Kang <engineerjkk@naver.com>
2024-12-22 19:35:00 +0900

    [Silver IV] Title: 수강변경, Time: 2380 ms, Memory: 261304 KB -BaekjoonHub

---
+++ 백준/Silver/23305. 수강변경/수강변경.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+dic={}
+for i in range(n):
+    if a[i] not in dic:
+        dic[a[i]]=[0,0]
+    dic[a[i]][0]+=1
+
+    if b[i] not in dic:
+        dic[b[i]]=[0,0]
+    dic[b[i]][1]+=1
+
+answer=0
+for key,value in dic.items():
+    supply=value[0]
+    demand=value[1]
+    answer+=min(supply,demand)
+print(n-answer)
+    
