76e6026f007da6c72c3ce0c43976b226c156c398
JuneKoo Kang <engineerjkk@naver.com>
2024-12-26 15:17:32 +0900

    [Bronze I] Title: 싸이클, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2526. 싸이클/싸이클.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+N,P=map(int,input().split())
+n=N
+order={}
+count=0
+
+while True:
+    if n not in order:
+        order[n]=count
+        n=n*N%P
+        count+=1
+    else:
+        print(count-order[n])
+        break
