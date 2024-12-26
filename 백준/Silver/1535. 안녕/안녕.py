e4e2df4364286dcf5bb223caad2ce673dde73520
JuneKoo Kang <engineerjkk@naver.com>
2024-12-26 21:34:02 +0900

    [Silver II] Title: 안녕, Time: 184 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -1,14 +1,17 @@
+import sys
+input = sys.stdin.readline
 from itertools import combinations
 
 N=int(input())
 L=list(map(int,input().split()))
 J=list(map(int,input().split()))
+
 max_sum=0
-for i in range(0,N+1):
-    for combination in combinations(range(N),i):
+for i in range(1,N+1):
+    for nCr in combinations(range(N),i):
         lsum=0
         jsum=0
-        for j in combination:
+        for j in nCr:
             lsum+=L[j]
             jsum+=J[j]
         if lsum<100:
