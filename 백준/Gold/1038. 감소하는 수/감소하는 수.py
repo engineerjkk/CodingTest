3580ea6d305af0d1945e03f990dfe2d377a2993f
JuneKoo Kang <engineerjkk@naver.com>
2024-12-27 17:55:43 +0900

    [Gold V] Title: 감소하는 수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.py
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -1,19 +1,18 @@
 import sys
 input = sys.stdin.readline
 from itertools import combinations
+
 n=int(input())
 
-
-a=[]
+answer=[]
 for i in range(1,11):
     for nCr in combinations(list(reversed(range(10))),i):
         tmp=''
         for j in nCr:
             tmp+=str(j)
-        a.append(int(tmp))
-
-a.sort()
-if len(a)<=n:
+        answer.append(int(tmp))
+answer.sort()
+if len(answer)<=n:
     print(-1)
 else:
-    print(a[n])
+    print(answer[n])
