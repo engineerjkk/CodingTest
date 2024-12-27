35f52b528f9a2b92d664e90d13b22fc11a906c16
JuneKoo Kang <engineerjkk@naver.com>
2024-12-28 00:01:45 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 124 ms, Memory: 37992 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,27 +1,27 @@
 import sys
-input = sys.stdin.readline
+input= sys.stdin.readline
 from collections import deque
 
 n,k=map(int,input().split())
+MAX=100000
+visit=[-1]*(MAX+1)
 
-visit=[-1]*100001
-
+visit[n]=0
 queue=deque()
 queue.append(n)
-visit[n]=0
-
 while queue:
     r=queue.popleft()
 
     if r==k:
         print(visit[r])
         exit()
-    if 0<=2*r<=100000 and visit[2*r]==-1:
+    
+    if 0<=2*r<=MAX and visit[2*r]==-1:
         queue.appendleft(2*r)
         visit[2*r]=visit[r]
     
     for nr in (r-1,r+1):
-        if 0<=nr<=100000 and visit[nr]==-1:
+        if 0<=nr<=MAX and visit[nr]==-1:
             queue.append(nr)
             visit[nr]=visit[r]+1
 print(-1)
