61e1bf40fe4d6c0ed7c70123b9bbf56f2fe7a8de
JuneKoo Kang <engineerjkk@naver.com>
2024-12-27 14:17:08 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -4,9 +4,7 @@ input = sys.stdin.readline
 n=int(input())
 string=input().strip()
 
-myhash=[31]*26
-
 answer=0
 for i,alpha in enumerate(string):
     answer+=(ord(alpha)-ord('a')+1)*(31**i)
-print(answer)
+print(answer%1234567891)
