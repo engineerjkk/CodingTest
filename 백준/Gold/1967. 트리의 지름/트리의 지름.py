7be8aa2cc37ac66adde25b34c5660b2a56c39cbc
JuneKoo Kang <engineerjkk@naver.com>
2025-01-01 21:15:08 +0900

    [Gold IV] Title: 트리의 지름, Time: 68 ms, Memory: 37752 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,25 +1,44 @@
-def find(start, now): # 가장 먼 지점 찾기
-    for a, b in tree[start]:
-        if visited[a] == -1:
-            visited[a] = b + now
-            find(a, visited[a])
-
 import sys
+input = sys.stdin.readline
 sys.setrecursionlimit(1000000)
-n = int(sys.stdin.readline())
-tree = [[] for _ in range(n+1)]
+n=int(input())
+
+graph=[[] for _ in range(n)]
 
 for _ in range(n-1):
-    p, c, w = map(int, sys.stdin.readline().split())
-    tree[p].append([c,w])
-    tree[c].append([p,w])
+    u,v,w=map(int,input().split())
+    u-=1
+    v-=1
+    graph[u].append((v,w))
+    graph[v].append((u,w))
 
-visited = [-1]*(n+1)
-visited[1] = 0
-find(1,0) # 가장 먼 지점을 먼저 찾고
+visit=[False]*n
+distance=[0]*n
 
-start = visited.index(max(visited))
-visited = [-1]*(n+1)
-visited[start] = 0 
-find(start,0) # 다시 가장 먼 지점을 찾아주자.
-print(max(visited))
+def dfs(u):
+    visit[u]=True
+
+    for v,w in graph[u]:
+        if not visit[v]:
+            distance[v]=distance[u]+w
+            dfs(v)
+
+dfs(0)
+
+max_distance=0
+who=0
+for i in range(n):
+    if max_distance<distance[i]:
+        max_distance=distance[i]
+        who=i
+
+visit=[False]*n
+distance=[0]*n
+dfs(who)
+
+max_distance=0
+for i in range(n):
+    if max_distance<distance[i]:
+        max_distance=distance[i]
+
+print(max_distance)
