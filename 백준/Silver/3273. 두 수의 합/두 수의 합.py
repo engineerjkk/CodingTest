42c4e98f6fc93c472839d62d12bbebc1379171dd
JuneKoo Kang <engineerjkk@naver.com>
2025-01-01 17:02:21 +0900

    [Silver III] Title: 두 수의 합, Time: 156 ms, Memory: 46620 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+lst=list(map(int,input().split()))
+x=int(input())
+
+count=[0]*1000001
+for i in range(n):
+    count[lst[i]]+=1
+
+answer=0
+for i in range((x+1)//2):
+    if x-i>=1000000:
+        continue
+    answer+=count[i]*count[x-i]
+if x%2==0:
+    answer+=count[x//2]*(count[x//2]-1)//2
+print(answer)
+
