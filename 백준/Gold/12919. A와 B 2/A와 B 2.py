a284997afdce422a5499d5d81534fe81e2d043af
JuneKoo Kang <engineerjkk@naver.com>
2025-01-04 12:18:41 +0900

    [Gold V] Title: A와 B 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,21 @@
+S=input()
+T=input()
+
+
+def dfs(t):
+    if len(t)==len(S):
+        return t==S
+
+    if t[-1]=='A':
+        if dfs(t[:-1]):
+            return True 
+    if t[0]=='B':
+        if dfs(t[1:][::-1]):
+            return True
+    
+    return False
+
+if dfs(T):
+    print(1)
+else:
+    print(0)
