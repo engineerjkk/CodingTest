b0d44778d0fdcbb09ab7659b8b46d7a414463b55
JuneKoo Kang <engineerjkk@naver.com>
2025-01-06 18:06:41 +0900

    [Silver I] Title: 에너지 모으기, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -4,22 +4,23 @@ input = sys.stdin.readline
 n=int(input())
 weights=list(map(int,input().split()))
 
-def collect_energy(weights):
+def solution(weights):
+
     n=len(weights)
 
     if n<=2:
         return 0 
     
     max_energy=0 
-
-    for i in range(1,n-1):
-        current_energy=weights[i-1]*weights[i+1]
-        new_weights=weights[:i]+weights[i+1:]
-        energy=current_energy+collect_energy(new_weights)
-        max_energy=max(max_energy,energy)
     
+    for i in range(1,n-1):
+        
+        current_energy=weights[i-1]*weights[i+1]
+        remain_energy=weights[:i]+weights[i+1:]
+        energy=current_energy+solution(remain_energy)
+        max_energy=max(max_energy,energy)
+
     return max_energy
 
-result=collect_energy(weights)
-print(result)
 
+print(solution(weights))
