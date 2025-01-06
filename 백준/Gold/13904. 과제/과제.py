3019bad5cf64bb2d89f50a39aef8579d0c5e18de
JuneKoo Kang <engineerjkk@naver.com>
2025-01-06 22:01:01 +0900

    [Gold III] Title: 과제, Time: 40 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.py
+++ 백준/Gold/13904. 과제/과제.py
@@ -1,10 +1,9 @@
-import sys
-input = sys.stdin.readline
+import sys 
+input = sys.stdin.readline 
 import heapq
 
 n=int(input())
 due=[[] for _ in range(1001)]
-
 for _ in range(n):
     d,w=map(int,input().split())
     due[d].append(w)
@@ -12,10 +11,9 @@ for _ in range(n):
 pq=[]
 answer=0
 for i in range(1000,0,-1):
-    
     for w in due[i]:
         heapq.heappush(pq,-w)
     
-    if len(pq)>0:
+    if pq:
         answer-=heapq.heappop(pq)
 print(answer)
