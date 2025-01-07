7aacec3792e70de3da84178206a85c787c2bc632
JuneKoo Kang <engineerjkk@naver.com>
2025-01-07 16:29:23 +0900

    [Bronze I] Title: 관광, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10041. 관광/관광.py
+++ 백준/Bronze/10041. 관광/관광.py
@@ -12,11 +12,11 @@ for i in range(n-1):
     x2,y2=p[i+1]
 
     if x1>x2:
-        x1,x2=x2,x1
-        y1,y2=y2,y1
-    
+        x1,x2=x2,x1 
+        y1,y2=y2,y1 
+
     if y1>y2:
-        answer+=(abs(x1-x2)+abs(y1-y2))
+        answer+=abs(x1-x2)+abs(y1-y2)
     else:
         answer+=max(abs(x1-x2),abs(y1-y2))
 print(answer)
