65dc90b24558e515e380af3be19fdc961f803d11
JuneKoo Kang <engineerjkk@naver.com>
2025-01-07 19:42:55 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
@@ -1,28 +1,13 @@
 import sys 
 input = sys.stdin.readline 
-from collections import deque 
 
 a,k=map(int,input().split())
-
-graph=[[] for _ in range(k+1)]
-
-for i in range(1,k+1):
-    if i+1<=k:
-        graph[i].append(i+1)
-    if i*2<=k:
-        graph[i].append(i*2)
-
-queue=deque()
-queue.append(a)
-visit=[False]*(k+1)
-visit[a]=True 
-step=[-1]*(k+1)
-step[a]=0
-while queue:
-    u=queue.popleft()
-    for v in graph[u]:
-        if not visit[v]:
-            visit[v]=True 
-            queue.append(v) 
-            step[v]=step[u]+1
-print(step[k])
+cnt=0
+while a!=k:
+    if k%2==0 and k//2>=a:
+        k//=2
+    else:
+        k-=1
+    cnt+=1
+print(cnt)
+    
