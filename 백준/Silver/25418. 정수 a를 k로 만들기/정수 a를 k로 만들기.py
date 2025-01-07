a1ef45be68773628a9e6c8e38ffdaf4748f87302
JuneKoo Kang <engineerjkk@naver.com>
2025-01-07 19:39:46 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 1164 ms, Memory: 222556 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
@@ -1,5 +1,5 @@
 import sys 
-input = sys.stdin.readline
+input = sys.stdin.readline 
 from collections import deque 
 
 a,k=map(int,input().split())
@@ -9,23 +9,20 @@ graph=[[] for _ in range(k+1)]
 for i in range(1,k+1):
     if i+1<=k:
         graph[i].append(i+1)
-    if 2*i<=k:
-        graph[i].append(2*i)
+    if i*2<=k:
+        graph[i].append(i*2)
 
-visit=[False]*(k+1)
-distance=[-1]*(k+1)
-queue=deque() 
-
+queue=deque()
 queue.append(a)
+visit=[False]*(k+1)
 visit[a]=True 
-distance[a]=0 
-
+step=[-1]*(k+1)
+step[a]=0
 while queue:
     u=queue.popleft()
     for v in graph[u]:
         if not visit[v]:
-            queue.append(v)
             visit[v]=True 
-            distance[v]=distance[u]+1 
-
-print(distance[k])
+            queue.append(v) 
+            step[v]=step[u]+1
+print(step[k])
