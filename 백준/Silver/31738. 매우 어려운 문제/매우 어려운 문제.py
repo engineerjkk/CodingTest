70a922b647374cf7bffcb88047600ad0ce18c83d
JuneKoo Kang <engineerjkk@naver.com>
2025-01-07 19:12:33 +0900

    [Silver V] Title: 매우 어려운 문제, Time: 1324 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/31738. 매우 어려운 문제/매우 어려운 문제.py
+++ 백준/Silver/31738. 매우 어려운 문제/매우 어려운 문제.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 
 n,m=map(int,input().split())
 
-if m<=n:
+if n>=m:
     print(0)
 else:
     answer=1
     for i in range(2,n+1):
-        answer*=i 
+        answer*=i
         answer%=m
     print(answer)
