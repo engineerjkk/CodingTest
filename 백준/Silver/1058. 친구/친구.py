3b82a4785e7b3e36bd96d74180874add98056081
JuneKoo Kang <engineerjkk@naver.com>
2025-01-09 12:09:52 +0900

    [Silver II] Title: 친구, Time: 72 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -9,24 +9,23 @@ for _ in range(n):
 
 max_count=0
 for i in range(n):
-    visit=[False]*n 
-    distance=[-1]*n 
     queue=deque()
     queue.append(i)
+    visit=[False]*n
     visit[i]=True 
-    distance[i]=0 
+    distance=[-1]*n
+    distance[i]=0
 
     while queue:
         u=queue.popleft()
         for v in range(n):
-            if friends[u][v]=='Y'and not visit[v]:
+            if friends[u][v]=='Y' and not visit[v]:
                 queue.append(v)
                 visit[v]=True 
-                distance[v]=distance[u]+1
-    
+                distance[v]=distance[u]+1 
     count=0
     for j in range(n):
-        if j!=i and distance[j] !=-1 and distance[j] <=2:
-            count+=1 
+        if i!=j and distance[j]!=-1 and distance[j]<=2:
+            count+=1
     max_count=max(max_count,count)
 print(max_count)
