b94e9821aa18dee936f7ea89eae310fba68ec561
JuneKoo Kang <engineerjkk@naver.com>
2025-01-09 17:27:36 +0900

    [Silver I] Title: 지름길, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -4,19 +4,17 @@ input = sys.stdin.readline
 n,d=map(int,input().split())
 shortcuts=[]
 for _ in range(n):
-    start,end,length=map(int,input().split())
-    shortcuts.append((start,end,length))
+    shortcuts.append(list(map(int,input().split())))
 
 dp=[]
 for i in range(d+1):
     dp.append(i)
 
-for i in range(d+1):
-    if i>0:
-        dp[i]=min(dp[i],dp[i-1]+1)
-
+for i in range(1,d+1):
+    
+    dp[i]=min(dp[i],dp[i-1]+1)
+    
     for start,end,length in shortcuts:
         if i==end and start<=end and end<=d:
             dp[i]=min(dp[i],dp[start]+length)
-
 print(dp[d])
