ee7b686c281b0ce2e13fb1422778964d62f5f109
engineerjkk <engineerjkk@naver.com>
2025-01-14 00:53:25 +0900

    250114

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -1,1 +1,15 @@
 import sys 
+input = sys.stdin.readline 
+n=int(input())
+
+answer=0
+while True:
+    if n%5==0:
+        answer+=n//5
+        break 
+    n-=2
+    answer+=1
+    if n<0:
+        print(-1)
+        exit()
+print(answer)
