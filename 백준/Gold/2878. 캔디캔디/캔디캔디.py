c3461a460e94aef8b322a0df9e1d7fca472bbd16
JuneKoo Kang <engineerjkk@naver.com>
2025-01-14 14:23:49 +0900

    [Gold II] Title: 캔디캔디, Time: 136 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/2878. 캔디캔디/캔디캔디.py
+++ 백준/Gold/2878. 캔디캔디/캔디캔디.py
@@ -1,12 +1,16 @@
-M, N = map(int, input().split())
-wants = []
-for _ in range(N):
-    wants.append(int(input()))
-wants.sort()
-cant = sum(wants) - M
-ans = 0
-for i in range(N):
-    tmp = min(wants[i], cant // (N-i))
-    ans += tmp**2
-    cant -= tmp
-print(ans % 2**64)
+import sys 
+input = sys.stdin.readline 
+
+m,n=map(int,input().split())
+candy=[]
+for _ in range(n):
+    candy.append(int(input()))
+
+candy.sort() 
+cant=sum(candy)-m
+answer=0
+for i in range(n):
+    tmp=min(candy[i],cant//(n-i))
+    answer+=tmp**2
+    cant-=tmp
+print(answer%2**64)
