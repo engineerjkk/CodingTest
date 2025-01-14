47c526d2fd771effdc284b02235bff01aa2fe4cd
JuneKoo Kang <engineerjkk@naver.com>
2025-01-14 16:31:26 +0900

    [Gold I] Title: 수영장 만들기, Time: 80 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/1113. 수영장 만들기/수영장 만들기.py
+++ 백준/Gold/1113. 수영장 만들기/수영장 만들기.py
@@ -1,50 +1,55 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
+import sys 
+input = sys.stdin.readline 
+from collections import deque 
 
-def bfs(i,j):
-    global answer
-    visited = [[0] * (M) for _ in range(N)]
-    height = pool[i][j]
-    min_height = 10
-    q = deque([(i,j)])
-    mini_pool = [(i,j)]
-    visited[i][j] = 1
+def in_range(r, c):
+    return 0 <= r < n and 0 <= c < m 
+
+def bfs(i, j, height, visit):
+    queue = deque([(i, j)])
+    visit[i][j] = True
+    count = 1  # 물이 고일 수 있는 칸의 수
+    flag = True  # 수영장이 될 수 있는지 체크
     
-    while q:
-        x, y = q.popleft()
-
+    while queue:
+        r, c = queue.popleft()
+        
         for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
+            nr = r + dr[k]
+            nc = c + dc[k]
+            
+            # 경계를 벗어나는 경우
+            if not in_range(nr, nc):
+                flag = False
+                continue
+            
+            # 현재 수위보다 낮거나 같고 아직 방문하지 않은 칸
+            if space[nr][nc] <= height and not visit[nr][nc]:
+                visit[nr][nc] = True
+                queue.append((nr, nc))
+                count += 1
+    
+    # 수영장이 될 수 있는 경우에만 칸 수 반환
+    return count if flag else 0
 
-            if nx in (-1,N) or ny in (-1,M):
-                return 0,0
+# 입력 받기
+n, m = map(int, input().split())
+space = []
+for _ in range(n):
+    space.append(list(map(int, input().strip())))
 
-            if height >= pool[nx][ny] and not visited[nx][ny]:
-                q.append((nx, ny))
-                mini_pool.append((nx,ny))
-                visited[nx][ny] = 1
-            elif height < pool[nx][ny]:
-                min_height = min(min_height, pool[nx][ny])
+# 방향 벡터
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
 
-    for ar in mini_pool:
-        x, y = ar
-        answer += (min_height - pool[x][y])
-        pool[x][y] = min_height
-
-
-
-N, M = map(int, input().split())
-pool = [list(map(int, input().rstrip())) for _ in range(N)]
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+# 높이를 1부터 8까지 올리면서 확인
 answer = 0
-
-
-for i in range(1, N-1):
-    for j in range(1, M-1):
-        bfs(i,j)
+for height in range(1, 9):
+    visit = [[False] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            # 현재 수위보다 낮고 아직 체크하지 않은 곳
+            if space[i][j] <= height and not visit[i][j]:
+                answer += bfs(i, j, height, visit)
 
 print(answer)
