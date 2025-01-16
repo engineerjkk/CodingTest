4b56f77abbcfaa636758cc3b4fd5b83e9abfb105
JuneKoo Kang <engineerjkk@naver.com>
2025-01-16 17:42:52 +0900

    [Gold II] Title: 가운데를 말해요, Time: 228 ms, Memory: 39016 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -1,24 +1,21 @@
-import sys
+import sys 
 input = sys.stdin.readline 
 import heapq
 
-n = int(input())
-left = []   # 중간값을 포함한 이전 값 최대힙
-right = []  # 중간값 이후의 값 최소힙
+left=[]
+right=[]
 
-for i in range(n):
-    x = int(input())
-
-    if len(left) == 0 or -left[0] >= x:
-        heapq.heappush(left, -x)
+n=int(input())
+for _ in range(n):
+    x=int(input())
+    if len(left)==0 or -left[0]>=x:
+        heapq.heappush(left,-x)
     else:
-        heapq.heappush(right, x)
-
-    if len(left) > len(right) + 1:
-        temp = -heapq.heappop(left)
-        heapq.heappush(right, temp)
-    elif len(right) > len(left):
-        temp = heapq.heappop(right)
-        heapq.heappush(left, -temp)
-
+        heapq.heappush(right,x)
+    
+    if len(left)>len(right)+1:
+        heapq.heappush(right,-heapq.heappop(left))
+    elif len(right)>len(left):
+        heapq.heappush(left,-heapq.heappop(right))
+    
     print(-left[0])
