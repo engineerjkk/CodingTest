288a5e46bb5f131f9bbfcfd97322dfb25ef09429
JuneKoo Kang <engineerjkk@naver.com>
2025-01-16 15:18:50 +0900

    [Gold IV] Title: 4연산, Time: 56 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/14395. 4연산/4연산.py
+++ 백준/Gold/14395. 4연산/4연산.py
@@ -3,28 +3,28 @@ input = sys.stdin.readline
 from collections import deque 
 
 s,t=map(int,input().split())
-visit=set() 
-path={}
-queue=deque() 
 
+visit=set() 
+queue=deque()
 queue.append(s)
 visit.add(s)
-path[s]=""
+path={}
+path[s]=''
 
 while queue:
-    u=queue.popleft() 
+    u=queue.popleft()
     if u*u <=1e9 and u*u not in visit:
         queue.append(u*u)
         visit.add(u*u)
-        path[u*u]=path[u]+"*"
+        path[u*u]=path[u]+'*'
     if u+u <=1e9 and u+u not in visit:
         queue.append(u+u)
         visit.add(u+u)
-        path[u+u]=path[u]+"+"
+        path[u+u]=path[u]+'+'
     if u/u <=1e9 and u/u not in visit:
         queue.append(u/u)
         visit.add(u/u)
-        path[u/u]=path[u]+"/"
+        path[u/u]=path[u]+'/'
 
 if s==t:
     print(0)
