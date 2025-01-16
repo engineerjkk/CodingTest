2c54f4e87faf8ff6900957e6ee41ea50439f4150
JuneKoo Kang <engineerjkk@naver.com>
2025-01-16 14:40:51 +0900

    [Gold V] Title: ABCDE, Time: 640 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,46 +1,37 @@
-def dfs(graph, visited, current, depth, start):
-    # 길이가 4인 경로를 찾았다면
-    if depth == 4:
-        return True
-        
-    # 현재 정점에서 연결된 모든 정점을 확인
-    for next_vertex in graph[current]:
-        # 이미 방문한 정점은 건너뛰기
-        if visited[next_vertex]:
-            continue
-            
-        # 다음 정점 방문
-        visited[next_vertex] = True
-        if dfs(graph, visited, next_vertex, depth + 1, start):
-            return True
-        visited[next_vertex] = False
-        
-    return False
+import sys 
+input = sys.stdin.readline 
 
-def solution(N, M, edges):
-    # 그래프 생성
-    graph = [[] for _ in range(N)]
-    for a, b in edges:
+def dfs(graph,visit,u,depth,start):
+    
+    if depth==4:
+        return True 
+    
+    for v in graph[u]:
+        if visit[v]:
+            continue 
+        
+        visit[v]=True 
+        if dfs(graph,visit,v,depth+1,start):
+            return True 
+        visit[v]=False 
+    return False 
+
+def solution(n,m,edges):
+    graph=[[] for _ in range(n)]
+    for i in range(m):
+        a,b=edges[i]
         graph[a].append(b)
         graph[b].append(a)
-        
-    # 각 정점을 시작점으로 하여 DFS 수행
-    for start in range(N):
-        visited = [False] * N
-        visited[start] = True
-        # 길이가 4인 경로를 찾았다면 1 반환
-        if dfs(graph, visited, start, 0, start):
+    
+    for i in range(n):
+        visit=[False]*n
+        visit[i]=True 
+        if dfs(graph,visit,i,0,i):
             return 1
-            
-    # 길이가 4인 경로를 찾지 못했다면 0 반환
     return 0
+n,m=map(int,input().split())
+edges=[]
+for _ in range(m):
+    edges.append(list(map(int,input().split())))
 
-# 입력 처리
-N, M = map(int, input().split())
-edges = []
-for _ in range(M):
-    a, b = map(int, input().split())
-    edges.append((a, b))
-
-# 결과 출력
-print(solution(N, M, edges))
+print(solution(n,m,edges))
