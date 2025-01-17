0c042f60a3d1aee3a5f0d3f0cb9a2d8a14bcdf36
JuneKoo Kang <engineerjkk@naver.com>
2025-01-17 10:57:35 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -7,10 +7,8 @@ def power(a,b,c):
     if b==1:
         return a%c 
     if b%2==0:
-        return (power(a,b//2,c)**2)%c
+        return (power(a,b//2,c)**2)%c 
     else:
         return ((power(a,b//2,c)**2)*a)%c
 
 print(power(a,b,c))
-    
-    
