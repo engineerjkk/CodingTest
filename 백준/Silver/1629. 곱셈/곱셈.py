4d9204eefda92a6d023e627f3560c398d31ffd89
JuneKoo Kang <engineerjkk@naver.com>
2025-01-17 10:52:24 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,16 @@
+import sys 
+input = sys.stdin.readline 
+
+a,b,c=map(int,input().split())
+
+def power(a,b,c):
+    if b==1:
+        return a%c 
+    if b%2==0:
+        return (power(a,b//2,c)**2)%c
+    else:
+        return ((power(a,b//2,c)**2)*a)%c
+
+print(power(a,b,c))
+    
+    
