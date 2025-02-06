0959fe6e893668404191f0884355936d1908c6eb
JuneKoo Kang <engineerjkk@naver.com>
2025-02-07 00:29:59 +0900

    [Gold III] Title: 파티, Time: 48 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,44 +1,36 @@
-import heapq
-import sys
-input = sys.stdin.readline
+import sys 
+input = sys.stdin.readline 
+import heapq 
 
-def dijkstra(start, graph, N):
-    distances = [float('inf')] * (N + 1)
-    distances[start] = 0
-    queue = []
-    heapq.heappush(queue, (0, start))
+n,m,x=map(int,input().split())
+graph=[[] for _ in range(n+1)]
+reverse_graph=[[] for _ in range(n+1)]
 
-    while queue:
-        curr_dist, curr = heapq.heappop(queue)
-        
-        if distances[curr] < curr_dist:
+for _ in range(m):
+    start,end,time=map(int,input().split())
+    graph[start].append((end,time))
+    reverse_graph[end].append((start,time))
+
+def dijkstra(start,graph,n):
+    distances=[1e9]*(n+1)
+    distances[start]=0
+    pq=[]
+    heapq.heappush(pq,(0,start))
+    while pq:
+        curr_dist,curr=heapq.heappop(pq)
+        if distances[curr]<curr_dist:
             continue
-            
-        for next_node, weight in graph[curr]:  # 여기만 수정
-            distance = curr_dist + weight
-            
-            if distance < distances[next_node]:
-                distances[next_node] = distance
-                heapq.heappush(queue, (distance, next_node))
-                
+        for next_node,weight in graph[curr]:
+            distance=curr_dist+weight
+            if distance<distances[next_node]:
+                distances[next_node]=distance 
+                heapq.heappush(pq,(distance,next_node))
     return distances
 
-def solution():
-    N, M, X = map(int, input().split())
-    
-    # 리스트로 그래프 초기화
-    graph = [[] for _ in range(N + 1)]
-    reverse_graph = [[] for _ in range(N + 1)]
-    
-    # 리스트 방식으로 그래프 구성
-    for _ in range(M):
-        start, end, time = map(int, input().split())
-        graph[start].append((end, time))
-        reverse_graph[end].append((start, time))
-    
-    from_party = dijkstra(X, graph, N)
-    to_party = dijkstra(X, reverse_graph, N)
-    
-    return max(from_party[i] + to_party[i] for i in range(1, N + 1))
+to_party=dijkstra(x,graph,n)
+to_home=dijkstra(x,reverse_graph,n)
 
-print(solution())
+max_time=0
+for i in range(1,n+1):
+    max_time=max(max_time,to_party[i]+to_home[i])
+print(max_time)
