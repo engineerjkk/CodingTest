df1387a1bf636a1a0ac08a980c0606f451821dbc
JuneKoo Kang <engineerjkk@naver.com>
2025-02-06 18:42:52 +0900

    [Gold V] Title: 강의실 배정, Time: 348 ms, Memory: 74744 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,18 +1,19 @@
-import sys
-input = sys.stdin.readline
-import heapq
+import sys 
+input = sys.stdin.readline 
+import heapq 
 
-n = int(input())
+n=int(input())
 lst=[]
 for _ in range(n):
     lst.append(list(map(int,input().split())))
 lst.sort()
-room=[]
-heapq.heappush(room,lst[0][1])
+
+rooms=[]
+heapq.heappush(rooms,lst[0][1])
 for i in range(1,n):
-    if lst[i][0]<room[0]:
-        heapq.heappush(room,lst[i][1])
+    if lst[i][0]<rooms[0]:
+        heapq.heappush(rooms,lst[i][1])
     else:
-        heapq.heappop(room)
-        heapq.heappush(room,lst[i][1])
-print(len(room))
+        heapq.heappop(rooms)
+        heapq.heappush(rooms,lst[i][1])
+print(len(rooms))
