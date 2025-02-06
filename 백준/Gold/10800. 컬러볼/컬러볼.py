2a0598b194b9a5e886cb2a48c62cebcbf3d12df2
JuneKoo Kang <engineerjkk@naver.com>
2025-02-06 17:40:02 +0900

    [Gold II] Title: 컬러볼, Time: 260 ms, Memory: 158940 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.py
+++ 백준/Gold/10800. 컬러볼/컬러볼.py
@@ -1,24 +1,21 @@
-import sys
-input = sys.stdin.readline
-
+import sys 
+input = sys.stdin.readline 
 n=int(input())
 balls=[[] for _ in range(2001)]
-
-for i in range(n):
+for id in range(n):
     c,s=map(int,input().split())
-    balls[s].append((c,i))
+    balls[s].append((c,id))
 
-answer=[0]*n
-total=0
 count={}
+answer=[0]*n 
+total=0
 for i in range(1,2001):
     for c,id in balls[i]:
         if c not in count:
-            count[c]=0
+            count[c]=0 
         answer[id]=total-count[c]
     total+=i*len(balls[i])
-    for c,_ in balls[i]:
-        count[c]+=i
-
+    for c,id in balls[i]:
+        count[c]+=i 
 for i in range(n):
     print(answer[i])
