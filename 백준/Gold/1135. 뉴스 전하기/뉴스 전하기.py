e1058827206e53e07ee54df24a51ef5c1235924e
JuneKoo Kang <engineerjkk@naver.com>
2025-02-06 20:48:00 +0900

    [Gold II] Title: 뉴스 전하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
@@ -1,6 +1,5 @@
-import sys
-input = sys.stdin.readline
-
+import sys 
+input = sys.stdin.readline 
 n=int(input())
 parents=list(map(int,input().split()))
 
@@ -9,20 +8,15 @@ for i in range(1,n):
     child[parents[i]].append(i)
 
 def dfs(now):
-
     if not child[now]:
-        return 0
-    
+        return 0 
     times=[]
     for v in child[now]:
         times.append(1+dfs(v))
-    
     times.sort(reverse=True)
-
     max_time=0
     for i,t in enumerate(times):
         max_time=max(max_time,i+t)
-    
     return max_time
-
-print(dfs(0))
+answer=dfs(0)
+print(answer)
