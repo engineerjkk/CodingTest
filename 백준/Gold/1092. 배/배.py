99643bbcac6706e0d527ab1676074ab2aff2f960
JuneKoo Kang <engineerjkk@naver.com>
2025-02-06 18:08:47 +0900

    [Gold V] Title: 배, Time: 200 ms, Memory: 110920 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -1,31 +1,27 @@
-import sys
-input = sys.stdin.readline
-
+import sys 
+input = sys.stdin.readline 
 n=int(input())
 cranes=list(map(int,input().split()))
 m=int(input())
 boxes=list(map(int,input().split()))
 
-def solution(n,cranes,m,boxes):
-    cranes.sort(reverse=True)
-    boxes.sort(reverse=True)
-    time=0
+time=0
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
 
-    if cranes[0]<boxes[0]:
-        return -1
-    
-    while boxes:
-        time+=1
-        idx=0
-        for crane in cranes:
-            if len(boxes)==0:
+if cranes[0]<boxes[0]:
+    print(-1)
+    exit()
+
+while boxes:
+    time+=1
+    idx=0
+    for crane in cranes:
+        if len(boxes)==0:
+            break 
+        while idx<len(boxes):
+            if boxes[idx]<=crane:
+                boxes.pop(idx)
                 break
-            while idx<len(boxes):
-                if boxes[idx]<=crane:
-                    boxes.pop(idx)
-                    break
-                idx+=1
-    return time
-        
-
-print(solution(n,cranes,m,boxes))
+            idx+=1
+print(time)
