d7538126b28b6f73089ea7c9bdc7025f6ca5daf1
JuneKoo Kang <engineerjkk@naver.com>
2025-02-06 16:58:41 +0900

    [Gold V] Title: 실크로드, Time: 528 ms, Memory: 44924 KB -BaekjoonHub

--- 백준/Gold/10710. 실크로드/실크로드.py
+++ 백준/Gold/10710. 실크로드/실크로드.py
@@ -1,6 +1,5 @@
 import sys 
 input = sys.stdin.readline 
-
 n,m=map(int,input().split())
 D=[]
 for _ in range(n):
@@ -11,19 +10,15 @@ for _ in range(m):
 
 dp=[[1e9]*(n+1) for _ in range(m+1)]
 dp[0][0]=0
-
 for i in range(m):
     for j in range(n+1):
         if dp[i][j]==1e9:
             continue 
-
         dp[i+1][j]=min(dp[i+1][j],dp[i][j])
-
         if j<n:
-            next_fatigue=dp[i][j]+D[j]*C[i]
+            next_fatigue=dp[i][j]+C[i]*D[j]
             dp[i+1][j+1]=min(dp[i+1][j+1],next_fatigue)
-
 result=1e9
-for i in range(1,m+1):
+for i in range(m+1):
     result=min(result,dp[i][n])
 print(result)
