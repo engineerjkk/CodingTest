5e2b4963d0c17e95e0349185bfcbc897bc430db4
JuneKoo Kang <engineerjkk@naver.com>
2025-02-07 14:08:38 +0900

    [Gold IV] Title: 돌 그룹, Time: 1004 ms, Memory: 56708 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.py
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.py
@@ -3,13 +3,13 @@ input = sys.stdin.readline
 from collections import deque 
 
 a,b,c=map(int,input().split())
-s=a+b+c
+s=a+b+c 
 if s%3!=0:
     print(0)
 else:
-    visit=[[False]*s for _ in range(s)]
     queue=deque()
     queue.append((a,b))
+    visit=[[False]*s for _ in range(s)]
     visit[a][b]=True 
     while queue:
         a,b=queue.popleft()
@@ -17,11 +17,10 @@ else:
         d=[a,b,c]
         for i in range(3):
             x,y=d[i],d[(i+1)%3]
-
             if x==y:
                 continue 
             if x>y:
-                x,y=y,x
+                x,y=y,x 
             if not visit[x+x][y-x]:
                 visit[x+x][y-x]=True 
                 queue.append((x+x,y-x))
@@ -29,5 +28,4 @@ else:
         print(1)
     else:
         print(0)
-            
-
+        
