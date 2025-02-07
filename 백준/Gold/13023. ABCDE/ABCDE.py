4953ecf9d4124721accf2955d364b9a514caec20
JuneKoo Kang <engineerjkk@naver.com>
2025-02-07 16:30:46 +0900

    [Gold V] Title: ABCDE, Time: 636 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,37 +1,29 @@
 import sys 
 input = sys.stdin.readline 
 
-def dfs(graph,visit,u,depth,start):
-    
+n,m=map(int,input().split())
+graph=[[] for _ in range(n)]
+for _ in range(m):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(graph,visit,u,depth):
     if depth==4:
         return True 
-    
     for v in graph[u]:
         if visit[v]:
             continue 
-        
         visit[v]=True 
-        if dfs(graph,visit,v,depth+1,start):
+        if dfs(graph,visit,v,depth+1):
             return True 
-        visit[v]=False 
-    return False 
+        visit[v]=False
+    return False
 
-def solution(n,m,edges):
-    graph=[[] for _ in range(n)]
-    for i in range(m):
-        a,b=edges[i]
-        graph[a].append(b)
-        graph[b].append(a)
-    
-    for i in range(n):
-        visit=[False]*n
-        visit[i]=True 
-        if dfs(graph,visit,i,0,i):
-            return 1
-    return 0
-n,m=map(int,input().split())
-edges=[]
-for _ in range(m):
-    edges.append(list(map(int,input().split())))
-
-print(solution(n,m,edges))
+for i in range(n):
+    visit=[False]*n
+    visit[i]=True 
+    if dfs(graph,visit,i,0):
+        print(1)
+        exit()
+print(0)
